f388787bf7ff65ab0bb1f0e37a92197f86e53685
Daniel Scharrer <daniel@constexpr.org>
2010-07-18 17:14:59 +0000

    restless: use xydra.log for logging

--- org.xydra.restless/src/main/java/org/xydra/restless/Restless.java
+++ org.xydra.restless/src/main/java/org/xydra/restless/Restless.java
@@ -12,8 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.xydra.log.Logger;
+import org.xydra.log.LoggerFactory;
 
 
 /**
