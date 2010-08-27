4a2bd18b6348414654bbbc8ef2482a1d29b29e31
Max Völkel <max.voelkel@calpano.com>
2010-08-27 13:28:33 +0000

    Initial structure for webadmin

---
+++ org.xydra.webadmin/src/main/java/org/xydra/webadmin/WebadminApp.java
@@ -0,0 +1,41 @@
+package org.xydra.webadmin;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.xydra.annotations.RunsInAppEngine;
+import org.xydra.annotations.RunsInJava;
+import org.xydra.log.Logger;
+import org.xydra.log.LoggerFactory;
+import org.xydra.restless.Restless;
+import org.xydra.restless.RestlessExceptionHandler;
+
+
+@RunsInAppEngine
+@RunsInJava
+public class WebadminApp {
+	
+	public static final Logger log = LoggerFactory.getLogger(WebadminApp.class);
+	
+	public static void restless(String prefix) {
+		
+		Restless.addExceptionHandler(new RestlessExceptionHandler() {
+			
+			public boolean handleException(Throwable t, HttpServletRequest req,
+			        HttpServletResponse res) {
+				log.error(t.getLocalizedMessage(), t);
+				// eat all exceptions
+				return true;
+			}
+		});
+		
+		WebadminApp webadminApp = new WebadminApp();
+		Restless.addGet("/index.html", webadminApp, "indexHtml");
+		
+	}
+	
+	public void indexHtml() {
+		
+	}
+	
+}
