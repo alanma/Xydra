51457824780189334a293a4314829b8249df7fed
Daniel Scharrer <daniel@constexpr.org>
2010-09-29 15:28:14 +0000

    core: add some convenience functions to XType

--- org.xydra.core/src/main/java/org/xydra/core/model/XType.java
+++ org.xydra.core/src/main/java/org/xydra/core/model/XType.java
@@ -5,9 +5,42 @@ package org.xydra.core.model;
  * entity they refer to.
  * 
  */
-
 public enum XType {
 	
-	XREPOSITORY, XMODEL, XOBJECT, XFIELD
+	XREPOSITORY, XMODEL, XOBJECT, XFIELD;
+	
+	/**
+	 * @return the XType of entities containing entities of this XType or null
+	 *         for XREPOSITORY
+	 */
+	public XType getParentType() {
+		switch(this) {
+		case XMODEL:
+			return XREPOSITORY;
+		case XOBJECT:
+			return XMODEL;
+		case XFIELD:
+			return XOBJECT;
+		default:
+			return null;
+		}
+	}
+	
+	/**
+	 * @return the XType of entities contained in entities of this XType or null
+	 *         for XFIELD
+	 */
+	public XType getChildType() {
+		switch(this) {
+		case XREPOSITORY:
+			return XMODEL;
+		case XMODEL:
+			return XOBJECT;
+		case XOBJECT:
+			return XFIELD;
+		default:
+			return null;
+		}
+	}
 	
 }
