2959c727ec62a6327c2e61fe17ef45b5247b59ce
bjoern.kaidel@googlemail.com <bjoern.kaidel@googlemail.com>
2010-10-21 16:16:35 +0000

    XIDs are now created by using XX instead of getting the XIDProvider

--- org.xydra.core/src/main/java/org/xydra/examples/X5MinuteTutorial.java
+++ org.xydra.core/src/main/java/org/xydra/examples/X5MinuteTutorial.java
@@ -1,8 +1,8 @@
 package org.xydra.examples;
 import org.xydra.core.X;
+import org.xydra.core.XX;
 import org.xydra.core.model.XField;
 import org.xydra.core.model.XID;
-import org.xydra.core.model.XIDProvider;
 import org.xydra.core.model.XModel;
 import org.xydra.core.model.XObject;
 import org.xydra.core.model.XRepository;
@@ -15,28 +15,25 @@ public class X5MinuteTutorial {
         public static void main(String[] args) {
                 XRepository exampleRepo = X.createMemoryRepository();
                 
-                // getting an XIDProvider will save us the work of creating XIDs
-                // ourselves
-                XIDProvider idProvider = X.getIDProvider();
-                // creating a random and unique XID for our phonebook XModel
-                XID phonebookID = idProvider.createUniqueID();
+                // creating a random and unique XID for our phonebook XModel by using the helper class XX
+                XID phonebookID = XX.createUniqueID();
                 
                 // instead of creating a random XID, we will create one from a String
-                XID actorID = idProvider.fromString("exampleActor");
+                XID actorID = XX.toId("exampleActor");
                 
                 XModel phonebook = exampleRepo.createModel(actorID, phonebookID);
                 
                 // adding an object representing an entry in the phonebook for a person
                 // named "John"
-                XID johnID = idProvider.fromString("john");
+                XID johnID = XX.toId("john");
                 XObject johnEntry = phonebook.createObject(actorID, johnID);
                 
                 // adding fields to the john-XObject which will the name and phonenumber
                 // of "John"
-                XID nameID = idProvider.fromString("name");
+                XID nameID = XX.toId("name");
                 XField nameField = johnEntry.createField(actorID, nameID);
                 
-                XID phonenrID = idProvider.fromString("phonenr");
+                XID phonenrID = XX.toId("phonenr");
                 XField phonenrField = johnEntry.createField(actorID, phonenrID);
                 
                 // names can be represented by Strings -> create an XStringValue
