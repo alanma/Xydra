4cd601472177d27731cfc4f2b66d2e5f4674fde6
Max Völkel <max.voelkel@calpano.com>
2010-10-25 12:06:23 +0000

    Fix for GAE compatibility

--- org.xydra.log.gae/src/main/java/org/xydra/log/gae/JulLoggerFactory.java
+++ org.xydra.log.gae/src/main/java/org/xydra/log/gae/JulLoggerFactory.java
@@ -1,7 +1,5 @@
 package org.xydra.log.gae;
 
-import java.util.logging.LogManager;
-
 import org.xydra.log.ILoggerFactorySPI;
 import org.xydra.log.Logger;
 
@@ -10,7 +8,7 @@ public class JulLoggerFactory implements ILoggerFactorySPI {
 	
 	@Override
 	public Logger getLogger(String name) {
-		java.util.logging.Logger logger = LogManager.getLogManager().getLogger(name);
+		java.util.logging.Logger logger = java.util.logging.Logger.getLogger(name);
 		return new JulLogger(logger);
 	}
 	
