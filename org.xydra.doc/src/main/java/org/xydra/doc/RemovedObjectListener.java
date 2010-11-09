15ca32d48cf09cd49dba9b91e33b71f6e6c2b398
Bjoern.Kaidel@googlemail.com <Bjoern.Kaidel@googlemail.com@0ae76674-a2bc-2176-3712-945bbf24aeb5>
2010-11-09 16:06:37 +0000

    simple example listener for XydraEventManagement tutorial

---
+++ org.xydra.doc/src/main/java/org/xydra/doc/RemovedObjectListener.java
@@ -0,0 +1,22 @@
+package org.xydra.doc;
+
+import org.xydra.core.change.ChangeType;
+import org.xydra.core.change.XModelEvent;
+import org.xydra.core.change.XModelEventListener;
+
+public class RemovedObjectListener implements XModelEventListener {
+	
+	private int counter; //variable that will count how many XObjects are removed from the XModel
+	
+	@Override
+	public void onChangeEvent(XModelEvent event) {
+		if(event.getChangeType() == ChangeType.REMOVE) {
+			counter++; //only increment the counter if an XObject is removed!
+		}
+	}
+
+	//we'll need a method for getting the value of our counter variable
+	public int getRemovedObjectsCounter() {
+		return this.counter;
+	}
+}
