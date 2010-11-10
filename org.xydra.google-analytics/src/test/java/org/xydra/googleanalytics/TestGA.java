032b49b5e81d6c917d856c37f99a7ef076fd2675
Max Völkel <max.voelkel@calpano.com>
2010-11-10 13:27:40 +0000

    more tests

--- org.xydra.google-analytics/src/test/java/org/xydra/googleanalytics/TestGA.java
+++ org.xydra.google-analytics/src/test/java/org/xydra/googleanalytics/TestGA.java
@@ -1,6 +1,9 @@
 package org.xydra.googleanalytics;
 
 import org.junit.Test;
+import org.xydra.googleanalytics.logsink.GALogListener;
+import org.xydra.log.Logger;
+import org.xydra.log.LoggerFactory;
 
 
 public class TestGA {
@@ -22,4 +25,33 @@ public class TestGA {
 		tracker2.track(new FocusPoint("click2"), "-", new UserInfoImpl("example.com"), event);
 	}
 	
+	@Test
+	public void testEvent() {
+		Tracker tracker = new Tracker(GOOGLE_ANALYTICS_ID);
+		tracker.track(new FocusPoint("eventpage"), "-", new UserInfoImpl("test.xydra.org"),
+		        new GaEvent("the category", "the action", "the optionalLabel", 3));
+	}
+	
+	@Test
+	public void testLoggingEvent() {
+		
+		GALogListener logListener = new GALogListener(GOOGLE_ANALYTICS_ID, "test.xydra.org");
+		LoggerFactory.setLogListener(logListener);
+		Logger log = LoggerFactory.getLogger(TestGA.class);
+		log.debug("should not be logged in GA");
+		log.warn("should be logged in GA as warn");
+		log.error("should be logged in GA as error");
+		log.error("should be logged in GA as error with exception", new RuntimeException(
+		        "the runtime exception message"));
+		
+		try {
+			throw new RuntimeException("a thrown exception");
+		} catch(RuntimeException e) {
+			log.warn("catching the exception", e);
+		}
+		
+		Logger log2 = LoggerFactory.getLogger(UserInfoImpl.class);
+		log2.warn("should be logged in GA as warn from the UserInfoImpl log");
+		
+	}
 }
