f21f2e4c1296bc34c76919cedb469240db9bf1d4
Bjoern.Kaidel@gmail.com <bjoern.kaidel@googlemail.com>
2010-12-07 16:28:44 +0000

    added test for getModelSnapshots()

--- org.xydra.core/src/main/java/org/xydra/core/test/store/AbstractStoreTest.java
+++ org.xydra.core/src/main/java/org/xydra/core/test/store/AbstractStoreTest.java
@@ -8,8 +8,13 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.xydra.core.model.XAddress;
+import org.xydra.core.model.XBaseModel;
 import org.xydra.core.model.XID;
 import org.xydra.store.AutorisationException;
+import org.xydra.store.BatchedResult;
+import org.xydra.store.Callback;
+import org.xydra.store.QuotaException;
 import org.xydra.store.XydraStore;
 
 /**
@@ -32,12 +37,12 @@ public abstract class AbstractStoreTest {
 	abstract protected XydraStore getStore(); 
 	
 	/**
-	 * Stores an accountname with its correct passwordhash in the given XID/string
+	 * Stores a correct/existing accountname with its correct passwordhash in the given XID/string
 	 */
 	abstract protected void getCorrectUser(XID account, String passwordhash);
 	
 	/**
-	 * Stores an accountname with an incorrect passwordhash in the given XID/string
+	 * Stores a correct/existing accountname with an incorrect passwordhash in the given XID/string
 	 */
 	abstract protected void getIncorrectUser(XID account, String passwordhash);
 	
@@ -51,6 +56,28 @@ public abstract class AbstractStoreTest {
 	 */
 	abstract protected void getQuotaForBruteForce(long quota);
 	
+	/**
+	 * Returns an array of {@link XAddress XAddresses} of {@link XXModel XModels} the actor with the given
+	 * {@link XID} has at least read access to.
+	 * @param accountID 
+	 * @return an array of {@link XAddress XAddresses} of {@link XXModel XModels} the actor with the given
+	 * {@link XID} has at least read access to.
+	 */
+	abstract protected XAddress[] getModelAddresses(XID accountID);
+	
+	/**
+	 * Returns the {@link XAddress} of an {@link XModel} the given actor has no access to (not even read access!)
+	 * @param accountID
+	 * @return Returns the {@link XAddress} of an {@link XModel} the given actor has no access to 
+	 * 			(not even read access!)
+	 */
+	abstract protected XAddress getModelAddressWithoutAccess(XID accountID);
+	
+	/**
+	 * @return the {@link XAddress} of a not existing {@link XModel}
+	 */
+	abstract protected XAddress getNotExistingModelAddress();
+	
 	@Before
 	public void setUp()  {
 		this.store = this.getStore();
@@ -61,6 +88,9 @@ public abstract class AbstractStoreTest {
 		getQuotaForBruteForce(this.bfQuota);
 	}
 	
+	/**
+	 * Test for the checkLogin()-method
+	 */
 	@Test
 	public void testCheckLogin() {
 		TestCallback<Boolean> callback = new TestCallback<Boolean>();
@@ -68,26 +98,147 @@ public abstract class AbstractStoreTest {
 		//Testing a login that should succeed
 		this.store.checkLogin(correctUser, correctUserPass, callback);
 		
-		assertTrue(this.waitOnCallbackSuccess(callback));
+		assertTrue(this.waitOnCallback(callback));
 		assertEquals(callback.getEffect(), true);
-		assertEquals(callback.getException(), null);
+		assertNull(callback.getException());
 		
 		//Testing a login that should fail because of a wrong accountname-passwordhash combination
 		callback = new TestCallback<Boolean>();
 		
 		this.store.checkLogin(incorrectUser, incorrectUserPass, callback);
 		
-		assertTrue(this.waitOnCallbackFailure(callback));
+		assertFalse(this.waitOnCallback(callback));
 		assertEquals(callback.getEffect(), false);
 		assertNotNull(callback.getException());
 		assertTrue(callback.getException() instanceof AutorisationException);
+		
+		//Testing the quota exception
+		for(long l = 0; l < this.bfQuota + 5; l++) {
+			callback = new TestCallback<Boolean>();
+			
+			this.store.checkLogin(incorrectUser, incorrectUserPass, callback);
+		}
+		
+		//should now return a QuotaException, since we exceeded the quota for failed login attempts by at least 5
+		assertFalse(this.waitOnCallback(callback));
+		assertEquals(callback.getEffect(), false);
+		assertNotNull(callback.getException());
+		assertTrue(callback.getException() instanceof QuotaException);
 		
 		//TODO How to test for other exception types like ConnectionException, TimeoutException etc.?
 		
 	}
 	
+	/**
+	 * Test for the getModelSnapshots-method
+	 */
+	@Test
+	public void testGetModelSnapshots() {
+		XAddress[] modelAddresses = this.getModelAddresses(this.correctUser);
+		XAddress noAccess = this.getModelAddressWithoutAccess(this.correctUser);
+		XAddress doesntExist = this.getNotExistingModelAddress();
+		TestCallback<BatchedResult<XBaseModel>[]> callback;
+		
+		//Test if it behaves correctly for wrong account + password combinations
+		callback = new TestCallback<BatchedResult<XBaseModel>[]>();
+		this.store.getModelSnapshots(this.incorrectUser, this.incorrectUserPass, modelAddresses, callback);
+		assertFalse(this.waitOnCallback(callback));
+		assertNull(callback.getEffect());
+		assertNotNull(callback.getException());
+		assertTrue(callback.getException() instanceof AutorisationException);
+		
+		//Test if it behaves correctly for addresses of XModels the user has access to
+		callback = new TestCallback<BatchedResult<XBaseModel>[]>();
+		this.store.getModelSnapshots(this.correctUser, this.correctUserPass, modelAddresses, callback);
+		assertTrue(this.waitOnCallback(callback));
+		assertNotNull(callback.getEffect());
+		assertNull(callback.getException());
+		
+		BatchedResult<XBaseModel>[] result = callback.getEffect();
+		assertEquals(result.length, modelAddresses.length);
+		
+		//check order of returned snapshots
+		for(int i = 0; i < modelAddresses.length; i++) {
+			assertEquals(modelAddresses[i], result[i].getResult());
+		}
+		
+		//Test if it behaves correctly for addresses of XModels the user has no access to
+		callback = new TestCallback<BatchedResult<XBaseModel>[]>();
+		XAddress[] tempArray = new XAddress[]{noAccess};
+		this.store.getModelSnapshots(this.correctUser, this.correctUserPass, tempArray, callback);
+		assertTrue(this.waitOnCallback(callback));
+		assertNotNull(callback.getEffect());
+		assertNull(callback.getException());
+		
+		result = callback.getEffect();
+		assertNull(result[0].getResult());
+		
+		//Test if it behaves correctly for addresses of XModels that don't exist
+		callback = new TestCallback<BatchedResult<XBaseModel>[]>();
+		tempArray = new XAddress[]{doesntExist};
+		this.store.getModelSnapshots(this.correctUser, this.correctUserPass, tempArray, callback);
+		assertTrue(this.waitOnCallback(callback));
+		assertNotNull(callback.getEffect());
+		assertNull(callback.getException());
+		
+		result = callback.getEffect();
+		assertNull(result[0].getResult());
+		
+		//Test if it behaves correctly for mixes of the cases above
+		callback = new TestCallback<BatchedResult<XBaseModel>[]>();
+		tempArray = new XAddress[modelAddresses.length+2];
+		System.arraycopy(modelAddresses, 0, tempArray, 0, modelAddresses.length);
+		tempArray[modelAddresses.length] = doesntExist;
+		tempArray[modelAddresses.length+1] = noAccess;
+		
+		this.store.getModelSnapshots(this.correctUser, this.correctUserPass, tempArray, callback);
+		assertTrue(this.waitOnCallback(callback));
+		assertNotNull(callback.getEffect());
+		assertNull(callback.getException());
+		
+		result = callback.getEffect();
+		assertEquals(result.length, tempArray.length);
+		
+		//check order of returned snapshots
+		for(int i = 0; i < modelAddresses.length; i++) {
+			if(i == modelAddresses.length) {
+				assertNull(result[i].getResult());
+			}
+			else if(i == modelAddresses.length + 1) {
+				assertNull(result[i].getResult());
+			}
+			else {
+				assertEquals(modelAddresses[i], result[i].getResult());
+			}		
+		}
+		
+		//TODO Maybe test more complex mixes?
+		
+		//Testing the quota exception
+		for(long l = 0; l < this.bfQuota + 5; l++) {
+			callback = new TestCallback<BatchedResult<XBaseModel>[]>();
+			tempArray = new XAddress[]{doesntExist}; //use small array to speed up the test
+			
+			this.store.getModelSnapshots(this.incorrectUser, this.incorrectUserPass, tempArray, callback);
+		}
+		
+		//should now return a QuotaException, since we exceeded the quota for failed login attempts by at least 5
+		assertFalse(this.waitOnCallback(callback));
+		assertNull(callback.getEffect());
+		assertNotNull(callback.getException());
+		assertTrue(callback.getException() instanceof QuotaException);
+		
+		//TODO How to test for other exception types like ConnectionException, TimeoutException etc.?
+	}
 	
-	private boolean waitOnCallbackSuccess(TestCallback<?> callback) {
+	/**
+	 * Method for checking whether a callback succeeded or not. Waits until the operation/method the callback was
+	 * passed to is finished or aborted by an error.
+	 * @param callback 
+	 * @return True, if the method which the callback was passed to succeeded, false if it failed or some kind
+	 * 			of error occurred
+	 */
+	private boolean waitOnCallback(TestCallback<?> callback) {
 		int value = callback.waitOnCallback(this.timeout);
 		if(value == TestCallback.UNKNOWN_ERROR) {
 			return false;
@@ -98,17 +249,4 @@ public abstract class AbstractStoreTest {
 		
 		return value == TestCallback.SUCCESS;
 	}
-	
-	private boolean waitOnCallbackFailure(TestCallback<?> callback) {
-		int value = callback.waitOnCallback(this.timeout);
-		if(value == TestCallback.UNKNOWN_ERROR) {
-			return false;
-		}
-		if(value == TestCallback.TIMEOUT) {
-			return false;
-		}
-		
-		return value == TestCallback.FAILURE;
-	}
-
 }
