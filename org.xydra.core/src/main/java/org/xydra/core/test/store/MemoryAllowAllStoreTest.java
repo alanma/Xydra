8d1735ac971f0b02a1cecedf2605bc7ddadfc80d
Bjoern.Kaidel@gmail.com <bjoern.kaidel@googlemail.com>
2010-12-22 18:15:17 +0000

    added a test for getRepositoryId

--- org.xydra.core/src/main/java/org/xydra/core/test/store/MemoryAllowAllStoreTest.java
+++ org.xydra.core/src/main/java/org/xydra/core/test/store/MemoryAllowAllStoreTest.java
@@ -1,8 +1,15 @@
 package org.xydra.core.test.store;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 import org.xydra.core.X;
 import org.xydra.core.XX;
 import org.xydra.core.change.XCommandFactory;
+import org.xydra.core.model.XID;
 import org.xydra.store.XydraStore;
 import org.xydra.store.impl.memory.MemoryNoAccessRightsNoBatchNoAsyncStore;
 import org.xydra.store.impl.memory.SynchronousNoAccessRightsStore;
@@ -10,12 +17,13 @@ import org.xydra.store.impl.memory.SynchronousNoAccessRightsStore;
 
 public class MemoryAllowAllStoreTest extends AbstractAllowAllStoreTest {
 	private XydraStore store;
+	private XID repositoryID = XX.createUniqueID();
 	
 	@Override
 	protected XydraStore getStore() {
 		if(this.store == null) {
 			this.store = getNewStore(new SynchronousNoAccessRightsStore(
-			        new MemoryNoAccessRightsNoBatchNoAsyncStore(XX.createUniqueID())));
+			        new MemoryNoAccessRightsNoBatchNoAsyncStore(this.repositoryID)));
 		}
 		
 		return this.store;
@@ -24,6 +32,24 @@ public class MemoryAllowAllStoreTest extends AbstractAllowAllStoreTest {
 	@Override
 	protected XCommandFactory getCommandFactory() {
 		return X.getCommandFactory();
+	}
+	
+	/*
+	 * Tests for getRepositoryID
+	 */
+	@Test
+	public void testGetRepositoryID() {
+		XID correctUser = this.getCorrectUser();
+		String correctUserPass = this.getCorrectUserPasswordHash();
+		
+		SynchronousTestCallback<XID> callback = new SynchronousTestCallback<XID>();
+		
+		this.store.getRepositoryId(correctUser, correctUserPass, callback);
+		
+		assertTrue(waitOnCallback(callback));
+		assertNotNull(callback.getEffect());
+		assertEquals(callback.getEffect(), this.repositoryID);
+		assertNull(callback.getException());
 	}
 	
 }
