121976e4a4930c70766702f4f5b62d2b2dca030f
Bjoern.Kaidel@gmail.com <bjoern.kaidel@googlemail.com>
2010-12-23 18:25:27 +0000

    Began writing a test for MemoryStore using a little workaround (see MemoryStore)

---
+++ org.xydra.core/src/main/java/org/xydra/core/test/store/MemoryStoreTest.java
@@ -0,0 +1,91 @@
+package org.xydra.core.test.store;
+
+import org.junit.Before;
+import org.xydra.core.X;
+import org.xydra.core.XX;
+import org.xydra.core.change.XCommandFactory;
+import org.xydra.core.model.XID;
+import org.xydra.store.XydraStore;
+import org.xydra.store.access.GroupModelWrapper;
+import org.xydra.store.impl.memory.MemoryStore;
+
+
+public class MemoryStoreTest extends AbstractStoreTest {
+	protected GroupModelWrapper gmw = null;
+	
+	@Override
+	protected XydraStore getStore() {
+		if(this.store != null) {
+			return this.store;
+		}
+		
+		this.store = new MemoryStore();
+		
+		return this.store;
+	}
+	
+	@Override
+	protected XCommandFactory getCommandFactory() {
+		return X.getCommandFactory();
+	}
+	
+	@Override
+	protected XID getCorrectUser() {
+		/*
+		 * FIXME this whole method propably needs to be changed, after Max tells
+		 * me how to actually work with the access rights here. ~Bjoern
+		 */
+		if(this.gmw == null) {
+			this.gmw = ((MemoryStore)this.getStore()).getGroupModelWrapper();
+		}
+		
+		XID actorId = XX.createUniqueID();
+		
+		if(!this.gmw.isValidLogin(actorId, this.getCorrectUserPasswordHash())) {
+			/*
+			 * FIXME addToGroup always fails!
+			 */
+			this.gmw.addToGroup(actorId, XX.toId("TestGroup"));
+			this.gmw.setPasswordHash(actorId, this.getCorrectUserPasswordHash());
+		}
+		System.out.println("LOL "
+		        + this.gmw.isValidLogin(actorId, this.getCorrectUserPasswordHash()));
+		
+		return actorId;
+	}
+	
+	@Override
+	protected String getCorrectUserPasswordHash() {
+		return "Test";
+	}
+	
+	@Override
+	protected XID getIncorrectUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	protected String getIncorrectUserPasswordHash() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	protected long getQuotaForBruteForce() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	@Before
+	public void setUp() {
+		if(this.setUpDone) {
+			return;
+		}
+		
+		super.setUp();
+		
+	}
+	
+}
