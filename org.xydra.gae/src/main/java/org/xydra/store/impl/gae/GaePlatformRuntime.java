e001002e2b762853b7a6251f775a06b631d1e0f3
Max Völkel <max.voelkel@calpano.com>
2011-01-24 14:29:52 +0000

    Added platform runtime

---
+++ org.xydra.gae/src/main/java/org/xydra/store/impl/gae/GaePlatformRuntime.java
@@ -0,0 +1,41 @@
+package org.xydra.store.impl.gae;
+
+import java.util.Collections;
+import java.util.Map;
+
+import javax.cache.Cache;
+import javax.cache.CacheException;
+import javax.cache.CacheFactory;
+import javax.cache.CacheManager;
+
+import org.xydra.base.XID;
+import org.xydra.log.Logger;
+import org.xydra.log.LoggerFactory;
+import org.xydra.store.XydraPlatformRuntime;
+import org.xydra.store.impl.delegate.XydraPersistence;
+
+
+public class GaePlatformRuntime implements XydraPlatformRuntime {
+	
+	private static final Logger log = LoggerFactory.getLogger(GaePlatformRuntime.class);
+	
+	@Override
+	public Map<Object,Object> getMemCache() {
+		try {
+			CacheFactory cacheFactory = CacheManager.getInstance().getCacheFactory();
+			Cache cache = cacheFactory.createCache(Collections.emptyMap());
+			@SuppressWarnings("unchecked")
+			Map<Object,Object> map = cache;
+			return map;
+		} catch(CacheException e) {
+			log.error("Could not create MemCache instance", e);
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@Override
+	public XydraPersistence getPersistence(XID repositoryId) {
+		return new GaePersistence(repositoryId);
+	}
+	
+}
