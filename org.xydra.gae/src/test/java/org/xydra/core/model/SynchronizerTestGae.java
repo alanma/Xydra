9dd328cb700879c135ddc5627b3053c0d2aaa6df
Max Völkel <max.voelkel@calpano.com>
2011-01-29 08:46:41 +0000

    Renamed all nonn-Tests into something different from Test.... or ...Test

--- org.xydra.gae/src/test/java/org/xydra/core/model/SynchronizerTestGae.java
+++ org.xydra.gae/src/test/java/org/xydra/core/model/SynchronizerTestGae.java
@@ -2,7 +2,7 @@ package org.xydra.core.model;
 
 import org.junit.BeforeClass;
 import org.xydra.base.XX;
-import org.xydra.core.TestLogger;
+import org.xydra.core.LoggerTestHelper;
 import org.xydra.core.model.state.XSPI;
 import org.xydra.core.model.state.impl.memory.TemporaryStateStore;
 import org.xydra.store.impl.delegate.DelegatingAllowAllStore;
@@ -14,7 +14,7 @@ public class SynchronizerTestGae extends AbstractSynchronizerTest {
 	
 	@BeforeClass
 	public static void init() {
-		TestLogger.init();
+		LoggerTestHelper.init();
 		GaeTestfixer.enable();
 		XSPI.setStateStore(new TemporaryStateStore());
 		actorId = XX.toId("tester");
