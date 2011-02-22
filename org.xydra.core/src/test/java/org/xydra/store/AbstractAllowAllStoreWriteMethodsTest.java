e1f6fbf3ce95d3855f695d17ff62177d521004ee
Bjoern.Kaidel@gmail.com <bjoern.kaidel@googlemail.com>
2011-02-22 15:17:01 +0000

    added new tests and began writing implementations (some are failing at the moment - will check that tomorrow)

---
+++ org.xydra.core/src/test/java/org/xydra/store/AbstractAllowAllStoreWriteMethodsTest.java
@@ -0,0 +1,68 @@
+package org.xydra.store;
+
+import org.xydra.base.XID;
+import org.xydra.base.XX;
+import org.xydra.store.impl.delegate.DelegatingAllowAllStore;
+import org.xydra.store.impl.delegate.XydraPersistence;
+
+
+/**
+ * This is an abstract class capsuling behaviour that is the same for all
+ * instantiations of {@link DelegatingAllowAllStore}.
+ * 
+ * (at the moment this is basically the same as
+ * {@link AbstractAllowAllStoreReadMethodsTest}, but may change in the future)
+ * 
+ * @author Kaidel
+ * 
+ */
+
+public abstract class AbstractAllowAllStoreWriteMethodsTest extends AbstractStoreWriteMethodsTest {
+	
+	/**
+	 * Every user can do everything, so it doesn't matter what we return here.
+	 * We use 'DirkCanDoAll' because that is easier in the debugger.
+	 */
+	@Override
+	protected XID getCorrectUser() {
+		return XX.toId("DirkCanDoAll");
+	}
+	
+	@Override
+	protected String getCorrectUserPasswordHash() {
+		return "Test";
+	}
+	
+	/**
+	 * Incorrect users do not exist, so we'll need to return null and do nothing
+	 * else
+	 */
+	@Override
+	protected XID getIncorrectUser() {
+		return null;
+	}
+	
+	@Override
+	protected String getIncorrectUserPasswordHash() {
+		return null;
+	}
+	
+	/**
+	 * Returns a new instance of AllowAllStore initialized with the given
+	 * {@link DelegatingAllowAllStore}. This makes it possible to reuse this
+	 * test with different instantiations.
+	 */
+	public DelegatingAllowAllStore getNewStore(XydraPersistence xydraPersistence) {
+		return new DelegatingAllowAllStore(xydraPersistence);
+	}
+	
+	/**
+	 * QuotaException cannot be tested here (no access rights mean no
+	 * QuotaException), so we can return what we want...
+	 */
+	@Override
+	protected long getQuotaForBruteForce() {
+		return 1;
+	}
+	
+}
---
+++ org.xydra.core/src/test/java/org/xydra/store/DelegatingAllowAllStoreWriteMethodsTest.java
@@ -0,0 +1,30 @@
+package org.xydra.store;
+
+import org.xydra.base.X;
+import org.xydra.base.XID;
+import org.xydra.base.change.XCommandFactory;
+import org.xydra.store.impl.memory.MemoryPersistence;
+
+
+public class DelegatingAllowAllStoreWriteMethodsTest extends AbstractAllowAllStoreWriteMethodsTest {
+	
+	@Override
+	protected XCommandFactory getCommandFactory() {
+		return X.getCommandFactory();
+	}
+	
+	@Override
+	protected XID getRepositoryId() {
+		return this.repoId;
+	}
+	
+	@Override
+	protected XydraStore getStore() {
+		if(this.store == null) {
+			this.store = getNewStore(new MemoryPersistence(this.repoId));
+		}
+		
+		return this.store;
+	}
+	
+}
