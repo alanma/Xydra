1b9a76f8c44753495583fb21c045dcb5df4680d0
Daniel Scharrer <daniel@constexpr.org>
2011-02-27 15:10:43 +0000

    xydra.gae: Add write methods test instantiation for GAE.

--- org.xydra.gae/src/test/java/org/xydra/store/GaeAllowAllStoreWriteMethodsTest.java
+++ org.xydra.gae/src/test/java/org/xydra/store/GaeAllowAllStoreWriteMethodsTest.java
@@ -2,7 +2,6 @@ package org.xydra.store;
 
 import org.junit.BeforeClass;
 import org.xydra.base.X;
-import org.xydra.base.XID;
 import org.xydra.base.XX;
 import org.xydra.base.change.XCommandFactory;
 import org.xydra.core.LoggerTestHelper;
@@ -10,24 +9,11 @@ import org.xydra.store.impl.gae.GaePersistence;
 import org.xydra.store.impl.gae.GaeTestfixer;
 
 
-public class GaeAllowAllStoreReadMethodsTest extends AbstractAllowAllStoreReadMethodsTest {
+public class GaeAllowAllStoreWriteMethodsTest extends AbstractAllowAllStoreWriteMethodsTest {
 	
 	@BeforeClass
 	public static void init() {
 		LoggerTestHelper.init();
-	}
-	
-	private XID repositoryId = XX.createUniqueID();
-	
-	@Override
-	protected XydraStore getStore() {
-		GaeTestfixer.enable();
-		
-		if(this.store == null) {
-			this.store = getNewStore(new GaePersistence(this.repositoryId));
-		}
-		
-		return this.store;
 	}
 	
 	@Override
@@ -36,8 +22,14 @@ public class GaeAllowAllStoreReadMethodsTest extends AbstractAllowAllStoreReadMe
 	}
 	
 	@Override
-	protected XID getRepositoryId() {
-		return this.repositoryId;
+	protected XydraStore getStore() {
+		GaeTestfixer.enable();
+		
+		if(this.store == null) {
+			this.store = getNewStore(new GaePersistence(XX.toId("data")));
+		}
+		
+		return this.store;
 	}
 	
 }
