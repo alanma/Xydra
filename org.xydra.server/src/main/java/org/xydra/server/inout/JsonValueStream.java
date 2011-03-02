6850bd408e7685f664e7e8313872b8b4e517f1e1
Max Völkel <max.voelkel@calpano.com>
2011-03-02 16:52:32 +0000

    Add CSV export for a Xydra repository, requires CSV module as a dependency

---
+++ org.xydra.server/src/main/java/org/xydra/server/inout/JsonValueStream.java
@@ -0,0 +1,31 @@
+package org.xydra.server.inout;
+
+import org.xydra.base.XAddress;
+import org.xydra.base.XID;
+import org.xydra.json.JSONWriter;
+
+
+/**
+ * TODO this class has never been tested/used yet.
+ * 
+ * @author xamde
+ * 
+ */
+public class JsonValueStream extends AbstractValueStream {
+	
+	@Override
+	public void address(XAddress address) {
+		this.buf.append("{address:" + encode(address.toString()) + "}");
+	}
+	
+	@Override
+	public void xid(XID a) {
+		this.buf.append("{xid:" + encode(a.toString()) + "}");
+	}
+	
+	@Override
+	public String encode(String s) {
+		return JSONWriter.quote(s);
+	}
+	
+}
