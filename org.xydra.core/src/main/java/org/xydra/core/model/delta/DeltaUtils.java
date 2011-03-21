e0a8aba470fe2154768babb185fbf14ec05bef5f
Max Völkel <max.voelkel@calpano.com>
2011-03-21 07:53:07 +0000

    More detailed logging

--- org.xydra.core/src/main/java/org/xydra/core/model/delta/DeltaUtils.java
+++ org.xydra.core/src/main/java/org/xydra/core/model/delta/DeltaUtils.java
@@ -174,7 +174,8 @@ public abstract class DeltaUtils {
 			        modelAddr.getModel(), rev - 1, inTrans));
 		}
 		
-		assert inTrans ^ (events.size() == 1);
+		assert inTrans ^ (events.size() == 1) : "inTrans?" + inTrans + " events.size=="
+		        + events.size();
 		
 		return events;
 	}
@@ -191,11 +192,11 @@ public abstract class DeltaUtils {
 		}
 		
 		for(XReadableObject object : changedModel.getNewObjects()) {
-			events.add(MemoryModelEvent.createAddEvent(actorId, changedModel.getAddress(), object
-			        .getID(), rev, inTrans));
+			events.add(MemoryModelEvent.createAddEvent(actorId, changedModel.getAddress(),
+			        object.getID(), rev, inTrans));
 			for(XID fieldId : object) {
-				DeltaUtils.createEventsForNewField(events, rev, actorId, object, object
-				        .getField(fieldId), inTrans);
+				DeltaUtils.createEventsForNewField(events, rev, actorId, object,
+				        object.getField(fieldId), inTrans);
 			}
 		}
 		
@@ -204,8 +205,8 @@ public abstract class DeltaUtils {
 			assert !implied;
 			
 			for(XID fieldId : object.getRemovedFields()) {
-				DeltaUtils.createEventsForRemovedField(events, rev, actorId, object, object
-				        .getOldField(fieldId), inTrans, false);
+				DeltaUtils.createEventsForRemovedField(events, rev, actorId, object,
+				        object.getOldField(fieldId), inTrans, false);
 			}
 			
 			for(XReadableField field : object.getNewFields()) {
@@ -245,8 +246,8 @@ public abstract class DeltaUtils {
 		events.add(MemoryObjectEvent.createAddEvent(actorId, object.getAddress(), field.getID(),
 		        rev, objectRev, inTrans));
 		if(!field.isEmpty()) {
-			events.add(MemoryFieldEvent.createAddEvent(actorId, field.getAddress(), field
-			        .getValue(), rev, objectRev, field.getRevisionNumber(), inTrans));
+			events.add(MemoryFieldEvent.createAddEvent(actorId, field.getAddress(),
+			        field.getValue(), rev, objectRev, field.getRevisionNumber(), inTrans));
 		}
 	}
 	
@@ -266,8 +267,8 @@ public abstract class DeltaUtils {
 	public static void createEventsForRemovedObject(List<XAtomicEvent> events, long modelRev,
 	        XID actorId, XReadableObject object, boolean inTrans, boolean implied) {
 		for(XID fieldId : object) {
-			DeltaUtils.createEventsForRemovedField(events, modelRev, actorId, object, object
-			        .getField(fieldId), inTrans, true);
+			DeltaUtils.createEventsForRemovedField(events, modelRev, actorId, object,
+			        object.getField(fieldId), inTrans, true);
 		}
 		events.add(MemoryModelEvent.createRemoveEvent(actorId, object.getAddress().getParent(),
 		        object.getID(), modelRev, object.getRevisionNumber(), inTrans, implied));
