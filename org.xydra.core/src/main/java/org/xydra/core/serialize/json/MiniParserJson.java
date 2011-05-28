45e2f95474136b754f49f737d3f70f2ec0b82b51
Daniel Scharrer <daniel@constexpr.org>
2011-05-28 10:10:31 +0000

    xydra.core: Remove overly verbose debug output from MiniParserJson.

--- org.xydra.core/src/main/java/org/xydra/core/serialize/json/MiniParserJson.java
+++ org.xydra.core/src/main/java/org/xydra/core/serialize/json/MiniParserJson.java
@@ -44,7 +44,6 @@ public class MiniParserJson implements MiniParser {
 		
 		@Override
 		public void arrayStart() {
-			System.out.println("arrayStart");
 			List<Object> c = new ArrayList<Object>();
 			onContainer(c);
 			this.list = c;
@@ -52,7 +51,6 @@ public class MiniParserJson implements MiniParser {
 		
 		@Override
 		public void objectStart() {
-			System.out.println("objectStart");
 			Map<String,Object> c = new HashMap<String,Object>();
 			onContainer(c);
 			this.map = c;
@@ -60,13 +58,11 @@ public class MiniParserJson implements MiniParser {
 		
 		@Override
 		public void arrayEnd() {
-			System.out.println("arrayEnd");
 			end();
 		}
 		
 		@Override
 		public void objectEnd() {
-			System.out.println("objectEnd");
 			end();
 		}
 		
@@ -86,43 +82,36 @@ public class MiniParserJson implements MiniParser {
 		
 		@Override
 		public void onKey(String key) {
-			System.out.println("onKey " + key);
 			this.key = key;
 		}
 		
 		@Override
 		public void onBoolean(boolean b) {
-			System.out.println("onBoolean " + b);
 			onObject(b);
 		}
 		
 		@Override
 		public void onDouble(double d) {
-			System.out.println("onDouble " + d);
 			onObject(d);
 		}
 		
 		@Override
 		public void onInteger(int i) {
-			System.out.println("onInteger " + i);
 			onObject(i);
 		}
 		
 		@Override
 		public void onLong(long l) {
-			System.out.println("onLong " + l);
 			onObject(l);
 		}
 		
 		@Override
 		public void onNull() {
-			System.out.println("onNull");
 			onObject(null);
 		}
 		
 		@Override
 		public void onString(String s) {
-			System.out.println("onString " + s);
 			onObject(s);
 		}
 		
