14692c98f0318b4efb227610e24a8c32de5b5aca
Max Völkel <max.voelkel@calpano.com>
2011-06-27 13:06:00 +0000

    Refactoring, documenting, ...

---
+++ org.xydra.testgae/src/test/java/org/xydra/testgae/RunXmasJetty.java
@@ -0,0 +1,44 @@
+package org.xydra.testgae;
+
+import java.io.File;
+import java.net.URI;
+
+import org.xydra.log.Logger;
+import org.xydra.log.LoggerFactory;
+import org.xydra.restless.Jetty;
+import org.xydra.server.impl.gae.CheckClasspathTool;
+import org.xydra.store.impl.gae.GaeTestfixer;
+
+
+/**
+ * This class is starts a Jetty server configured to allow testing of the
+ * webapp, loading static files directly from src/main/webapp. This class is not
+ * required to run the webapp.
+ * 
+ * @author voelkel
+ * 
+ */
+public class RunXmasJetty {
+	
+	private static final Logger log = LoggerFactory.getLogger(RunXmasJetty.class);
+	
+	public static void main(String[] args) throws Exception {
+		
+		/*
+		 * Enable tests with GAE (especially mail)
+		 */
+		GaeTestfixer.enable();
+		/* Make this thread GAE-test-ready */
+		GaeTestfixer.initialiseHelperAndAttachToCurrentThread();
+		
+		// start jetty
+		Jetty jetty = new Jetty(8787);
+		
+		// verify classpath has no weird errors
+		CheckClasspathTool.checkGaeToolsJarIsPresentAndAppstatsFilterClassCanBeLoaded();
+		
+		URI uri = jetty.startServer("", new File("src/main/webapp"));
+		
+		log.info("Started embedded Jetty server. User interface is at " + uri.toString());
+	}
+}
---
+++ org.xydra.testgae/src/test/java/org/xydra/testgae/client/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Code in this package is intended to run only form a test machine. It is
+ * issuing requests against a remote server.
+ */
+package org.xydra.testgae.client;
+
