2005f0af390e9a491472a29d074b24e8a8bf3b2c
Max Völkel <max.voelkel@calpano.com>
2011-07-03 00:32:30 +0000

    Added a (not yet well tested) runtime configuration system that works across multiple GAE instances while keeping extra communication minimal

---
+++ org.xydra.gae/src/main/java/org/xydra/gae/admin/GaeConfiguration.java
@@ -0,0 +1,121 @@
+package org.xydra.gae.admin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.xydra.store.IMemCache;
+import org.xydra.store.XydraRuntime;
+import org.xydra.store.impl.gae.GaeUtils;
+
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+
+/**
+ * A simple map-like configuration object that can be put in datastore and
+ * memcache. Has a time to live to minimize data store hits.
+ * 
+ * @author xamde
+ * 
+ */
+public class GaeConfiguration {
+	
+	public static final String PROP_VALID_UTC = "validUntilUTC";
+	
+	private static final long serialVersionUID = 1L;
+	private static final Key KEY_CONF = KeyFactory.createKey("XCONF", "GaeConfig");
+	
+	private Map<String,String> map = new HashMap<String,String>();
+	
+	private transient long validUntilUTC;
+	
+	public static GaeConfiguration createWithLifetime(long lifetimeInMs) {
+		GaeConfiguration conf = new GaeConfiguration();
+		conf.setValidUntilUTC(System.currentTimeMillis() + lifetimeInMs);
+		return conf;
+	}
+	
+	/**
+	 * @return a {@link GaeConfiguration} found in memcache or data store. If
+	 *         coming from memcache and too old, a fresh one is fetched.
+	 */
+	public static GaeConfiguration getInstance() {
+		IMemCache memcache = XydraRuntime.getMemcache();
+		GaeConfiguration conf = (GaeConfiguration)memcache.get(KEY_CONF);
+		if(conf == null || !conf.isStillValid()) {
+			// get fresh
+			conf = GaeConfiguration.load();
+			// store in memcache
+			memcache.put(KEY_CONF, conf);
+		}
+		assert conf != null;
+		return conf;
+	}
+	
+	private GaeConfiguration() {
+	}
+	
+	public void setValidUntilUTC(long validUntilUTC) {
+		this.map.put(PROP_VALID_UTC, "" + validUntilUTC);
+		this.validUntilUTC = validUntilUTC;
+	}
+	
+	public boolean isStillValid() {
+		return System.currentTimeMillis() < this.validUntilUTC;
+	}
+	
+	public static void store(GaeConfiguration conf) {
+		if(!conf.map.containsKey(PROP_VALID_UTC)) {
+			throw new IllegalStateException("Missing " + PROP_VALID_UTC);
+		}
+		Entity entity = new Entity(KEY_CONF);
+		for(String key : conf.map.keySet()) {
+			String value = conf.map.get(key);
+			entity.setUnindexedProperty(key, value);
+		}
+		GaeUtils.putEntityAsync(entity);
+	}
+	
+	public static GaeConfiguration load() {
+		Entity entity = GaeUtils.getEntity(KEY_CONF);
+		GaeConfiguration conf = new GaeConfiguration();
+		for(String key : entity.getProperties().keySet()) {
+			String value = (String)entity.getProperty(key);
+			conf.map.put(key, value);
+		}
+		if(!conf.map.containsKey(PROP_VALID_UTC)) {
+			throw new IllegalStateException("Missing " + PROP_VALID_UTC);
+		}
+		conf.validUntilUTC = Long.parseLong(conf.map.get(PROP_VALID_UTC));
+		return conf;
+	}
+	
+	public long getTimeToLive() {
+		return this.validUntilUTC - System.currentTimeMillis();
+	}
+	
+	public long getValidUntilUTC() {
+		return this.validUntilUTC;
+	}
+	
+	/**
+	 * @return the internal map
+	 */
+	public Map<String,String> map() {
+		return this.map;
+	}
+	
+	/**
+	 * A null is interpreted as false.
+	 * 
+	 * @param propName ..
+	 */
+	public boolean getAsBoolean(String propName) {
+		String value = this.map.get(propName);
+		if(value == null)
+			return false;
+		return Boolean.parseBoolean(value);
+	}
+	
+}
--- org.xydra.gae/src/main/java/org/xydra/gae/admin/GaeConfigurationResource.java
+++ org.xydra.gae/src/main/java/org/xydra/gae/admin/GaeConfigurationResource.java
@@ -2,14 +2,20 @@ package org.xydra.gae.admin;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.xydra.log.Logger;
+import org.xydra.log.LoggerFactory;
+import org.xydra.perf.StatsGatheringMemCacheWrapper;
+import org.xydra.perf.StatsGatheringPersistenceWrapper;
 import org.xydra.restless.Restless;
-import org.xydra.restless.RestlessParameter;
+import org.xydra.restless.utils.Clock;
 import org.xydra.restless.utils.HtmlUtils;
 import org.xydra.restless.utils.ServletUtils;
+import org.xydra.store.IMemCache;
 import org.xydra.store.XydraRuntime;
 import org.xydra.store.impl.gae.GaeAssert;
 import org.xydra.store.impl.gae.GaePersistence;
@@ -23,18 +29,42 @@ import org.xydra.store.impl.gae.GaeUtils;
  */
 public class GaeConfigurationResource {
 	
+	private static final Logger log = LoggerFactory.getLogger(GaeConfigurationResource.class);
+	
+	public static final String PROP_ASSERT = "assert";
+	
+	private static final String PROP_USEMEMCACHE = "usememcache";
+	
+	/** only used for incoming web request to clear memcache immediately */
+	private static final String PROP_CLEARMEMCACHE_NOW = "clearmemcache";
+	
+	private static GaeConfiguration currentConf = null;
+	
+	/** first 60 seconds after boot this config is valid */
+	public static final GaeConfiguration DEFAULT = GaeConfiguration.createWithLifetime(60 * 1000);
+	
+	static {
+		DEFAULT.map().put(PROP_ASSERT, "false");
+		DEFAULT.map().put(PROP_USEMEMCACHE, "true");
+		// no PROP_CLEARMEMCACHE_NOW
+		DEFAULT.map().put(XydraRuntime.PROP_MEMCACHESTATS, "false");
+		DEFAULT.map().put(XydraRuntime.PROP_PERSISTENCESTATS, "false");
+		currentConf = DEFAULT;
+	}
+	
 	/**
-	 * Clear the memcache.
+	 * @param r restless
+	 * @param path should be empty string
 	 */
-	public static void clearCache(HttpServletResponse res) throws IOException {
-		Writer w = startPage(GaePersistence.INSTANCE_ID, res);
-		XydraRuntime.getMemcache().clear();
-		w.write("Memcache cleared.");
+	public static void restless(Restless r, String path) {
+		r.addMethod(path + "/gaeconf/set", "GET", GaeConfigurationResource.class,
+		        "setConfiguration", true);
+		
+		r.addMethod(path + "/gaeconf", "GET", GaeConfigurationResource.class, "index", true);
 	}
 	
 	public static void index(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		GaeTestfixer.initialiseHelperAndAttachToCurrentThread();
-		ServletUtils.headers(res, "text/html");
 		Writer w = HtmlUtils.startHtmlPage(res, "GAE cache conf on instance "
 		        + GaePersistence.INSTANCE_ID);
 		w.write("<h2>GAE cache conf on instance " + GaePersistence.INSTANCE_ID + " </h2>");
@@ -62,25 +92,17 @@ public class GaeConfigurationResource {
 		w.write("<h3>Memcache Stats</h3> " + XydraRuntime.getMemcache().stats() + " Size: "
 		        + XydraRuntime.getMemcache().size() + "</p>");
 		
+		// FIXME ...
+		// --------------------------------------- stats
+		if(StatsGatheringPersistenceWrapper.isEnabled()) {
+			w.write(HtmlUtils.link("/admin/statsPersistence/stop", "Stop")
+			        + " stats-gathering persistence");
+		} else {
+			w.write(HtmlUtils.link("/admin/statsPersistence/start", "Stop")
+			        + " stats-gathering persistence");
+		}
+		
 		w.flush();
-	}
-	
-	/**
-	 * @param r restless
-	 * @param path should be empty string
-	 */
-	public static void restless(Restless r, String path) {
-		r.addMethod(path + "/gaeconf/memcache/set", "GET", GaeConfigurationResource.class,
-		        "setCacheConf", true, new RestlessParameter("instance", null),
-		        new RestlessParameter("memcache", null));
-		r.addMethod(path + "/gaeconf/memcache/clear", "GET", GaeConfigurationResource.class,
-		        "clearCache", true, new RestlessParameter("instance", null));
-		
-		r.addMethod(path + "/gaeconf/assert/set", "GET", GaeConfigurationResource.class,
-		        "setAssert", true, new RestlessParameter("instance", null), new RestlessParameter(
-		                "assert", null));
-		
-		r.addMethod(path + "/gaeconf", "GET", GaeConfigurationResource.class, "index", true);
 	}
 	
 	/**
@@ -93,41 +115,136 @@ public class GaeConfigurationResource {
 	}
 	
 	/**
-	 * @param memcache 'true' or 'false'
+	 * Set some parameters just for a specific instance
+	 * 
 	 * @throws IOException ...
 	 */
-	public static void setCacheConf(String instanceId, String memcache, HttpServletResponse res)
+	public static void setInstanceConfiguration(String instanceId, HttpServletResponse res)
 	        throws IOException {
-		Writer w = startPage(GaePersistence.INSTANCE_ID, res);
-		if(onRightInstance(instanceId)) {
-			boolean memcache_ = Boolean.parseBoolean(memcache);
-			GaeUtils.setUseMemCache(memcache_);
-			w.write("Memcache set to " + memcache + "<br />");
-		} else {
-			w.write("No action. Please retry and hope your request will hit the right instance.");
-		}
-	}
-	
-	public static void setAssert(String instanceId, String gaeAssert, HttpServletResponse res)
-	        throws IOException {
-		Writer w = startPage(GaePersistence.INSTANCE_ID, res);
-		if(onRightInstance(instanceId)) {
-			boolean gaeAssert_ = Boolean.parseBoolean(gaeAssert);
-			GaeAssert.setEnabled(gaeAssert_);
-			w.write("GaeAssert set to " + gaeAssert + "<br />");
-		} else {
-			w.write("No action. Please retry and hope your request will hit the right instance.");
-		}
-	}
-	
-	private static Writer startPage(String instanceId, HttpServletResponse res) throws IOException {
 		GaeTestfixer.initialiseHelperAndAttachToCurrentThread();
 		
-		ServletUtils.headers(res, "text/html");
-		Writer w = HtmlUtils.startHtmlPage(res, "GAE cache conf on instance "
+		Writer w = HtmlUtils.startHtmlPage(res, "Setting configuration on instance "
 		        + GaePersistence.INSTANCE_ID);
 		w.write("InstanceID: " + GaePersistence.INSTANCE_ID + "<br />");
 		w.write("Requested instance: " + instanceId + "<br />");
-		return w;
+		if(onRightInstance(instanceId)) {
+			w.write("Yeah, right instance");
+		} else {
+			w.write("No action. Please retry and hope your request will hit the right instance.");
+		}
 	}
+	
+	/**
+	 * Take all parameters found {@link HttpServletRequest} and put them in the
+	 * current configuration.
+	 * 
+	 * @throws IOException ...
+	 */
+	public static void setConfiguration(HttpServletRequest req, HttpServletResponse res)
+	        throws IOException {
+		GaeTestfixer.initialiseHelperAndAttachToCurrentThread();
+		
+		Clock c = new Clock().start();
+		Writer w = HtmlUtils.startHtmlPage(res, "Setting GAE Configuration");
+		Map<String,String> params = ServletUtils.getRequestparametersAsMap(req);
+		
+		if(params.containsKey(PROP_CLEARMEMCACHE_NOW)) {
+			w.write("Clearing memcache (effective for all instances)<br />");
+			w.flush();
+			XydraRuntime.getMemcache().clear();
+			w.write("Cleared memcache.<br />");
+			w.flush();
+		} else {
+			w.write("No memcache clear. Request one with &" + PROP_CLEARMEMCACHE_NOW + "<br />");
+			
+		}
+		
+		w.write("Loading current conf ...<br />");
+		w.flush();
+		GaeConfiguration conf = getCurrentConfiguration();
+		w.write("Stll valid for " + conf.getTimeToLive() + " ms (until " + conf.getValidUntilUTC()
+		        + ") -- and there is nothing (yet) you can do to speed this up<br />");
+		w.write("Updating conf ...<br />");
+		w.flush();
+		String validUntilUtcStr = params.get(GaeConfiguration.PROP_VALID_UTC);
+		if(validUntilUtcStr == null) {
+			throw new IllegalArgumentException("paramter '" + GaeConfiguration.PROP_VALID_UTC
+			        + "' must be set");
+		}
+		conf.setValidUntilUTC(Long.parseLong(validUntilUtcStr));
+		w.write("New config valid until " + conf.getValidUntilUTC() + " = expires in "
+		        + conf.getTimeToLive() + "+ms<br />");
+		w.flush();
+		for(String key : params.keySet()) {
+			if(key.equals(GaeConfiguration.PROP_VALID_UTC)) {
+				continue;
+			}
+			String value = params.get(key);
+			if(value == null) {
+				String previous = conf.map().remove(key);
+				w.write("Removing key '" + key + "' from config (was set to '" + previous
+				        + "' before)<br />");
+			} else {
+				String previous = conf.map().put(key, value);
+				w.write("Setting key '" + key + "' to '" + value + "' in config (was set to '"
+				        + previous + "' before)<br />");
+			}
+			w.flush();
+		}
+		w.write("Persisting conf... <br />");
+		w.flush();
+		persistsConfiguration(conf);
+		w.write("Processing conf (now all the stuff you said becomes effective - ON THIS INSTANCE only)<br />");
+		w.flush();
+		processConfiguration(conf);
+		w.write("Done with config update. Stats: " + c.stop("request").getStats());
+		HtmlUtils.endHtmlPage(w);
+	}
+	
+	private static void processConfiguration(GaeConfiguration conf) {
+		// assertions
+		boolean gaeAssert = conf.getAsBoolean(PROP_ASSERT);
+		GaeAssert.setEnabled(gaeAssert);
+		// memcache
+		boolean usememcache = Boolean.parseBoolean(PROP_USEMEMCACHE);
+		GaeUtils.setUseMemCache(usememcache);
+		// memcache stats
+		boolean memcachestats = Boolean.parseBoolean(XydraRuntime.PROP_MEMCACHESTATS);
+		setMemcacheStats(memcachestats);
+		// memcache stats
+		boolean persistencestats = Boolean.parseBoolean(XydraRuntime.PROP_PERSISTENCESTATS);
+		setPersistenceStats(persistencestats);
+	}
+	
+	private static void setMemcacheStats(boolean memcachestats) {
+		if(memcachestats) {
+			// force creating a new memcache
+			XydraRuntime.setMemcacheInstance(null);
+			IMemCache memcache = XydraRuntime.getMemcache();
+			// wrap
+			IMemCache statsMemcache = new StatsGatheringMemCacheWrapper(memcache);
+			XydraRuntime.setMemcacheInstance(statsMemcache);
+		} else {
+			// force creating a new memcache
+			XydraRuntime.setMemcacheInstance(null);
+		}
+	}
+	
+	private static void setPersistenceStats(boolean persistencestats) {
+		XydraRuntime.setParameter(XydraRuntime.PROP_PERSISTENCESTATS, "" + persistencestats);
+		XydraRuntime.forceReInitialisation();
+	}
+	
+	private static void persistsConfiguration(GaeConfiguration conf) {
+		GaeConfiguration.store(conf);
+	}
+	
+	public static synchronized GaeConfiguration getCurrentConfiguration() {
+		if(currentConf == null || !currentConf.isStillValid()) {
+			log.info("Current config is either null or too old. Getting a fresh one.");
+			currentConf = GaeConfiguration.getInstance();
+		}
+		return currentConf;
+	}
+	
 }
