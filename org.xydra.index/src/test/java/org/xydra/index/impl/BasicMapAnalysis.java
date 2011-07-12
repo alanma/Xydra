a1f03f5d8593011ae422ab96440ffc389a3be691
Max Völkel <max.voelkel@calpano.com>
2011-07-12 07:41:40 +0000

    Analysis of some maps' behaviour with respect to null keys.

---
+++ org.xydra.index/src/test/java/org/xydra/index/impl/BasicMapAnalysis.java
@@ -0,0 +1,24 @@
+package org.xydra.index.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+
+public class BasicMapAnalysis {
+	
+	public static void main(String[] args) {
+		/* How does a normal map handle null keys? It depends. */
+
+		testNullHandling(new HashMap<String,String>());
+		testNullHandling(new TreeMap<String,String>());
+	}
+	
+	public static void testNullHandling(Map<String,String> map) {
+		map.put(null, "test");
+		assert map.keySet() != null;
+		assert map.keySet().contains(null);
+		assert map.values().contains("test");
+	}
+	
+}
