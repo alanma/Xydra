391a341554c1da02c85511d87db1e4819e7d6075
Bjoern.Kaidel@gmail.com <bjoern.kaidel@googlemail.com>
2011-08-07 12:27:20 +0000

    added some simple tests

--- org.xydra.testgae/src/test/java/org/xydra/testgae/client/RemoteBenchmark.java
+++ org.xydra.testgae/src/test/java/org/xydra/testgae/client/RemoteBenchmark.java
@@ -1,15 +1,18 @@
 package org.xydra.testgae.client;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.Exchanger;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.xydra.testgae.shared.HttpUtils;
@@ -24,9 +27,15 @@ import org.xydra.testgae.shared.SimulatedUser;
  */
 public class RemoteBenchmark {
 	protected String absoluteUrl;
+	protected String dataUrl;
 	
-	@Test
+	@Before
+	public void setupBenchmark() {
+		
+	}
+	
 	@Ignore
+	@Test
 	public void testRunBenchmark() {
 		System.out.println("---- Running benchmark1 -----");
 		SimulatedUser u1 = new SimulatedUser(this.absoluteUrl, "repo1", new Exchanger<Exception>());
@@ -39,8 +48,8 @@ public class RemoteBenchmark {
 		
 	}
 	
-	@Test
 	@Ignore
+	@Test
 	public void testRunLoadTest() {
 		try {
 			runLoadTest(this.absoluteUrl, "repo2", 20, 3 * 60 * 1000);
@@ -92,6 +101,7 @@ public class RemoteBenchmark {
 		}
 	}
 	
+	@Ignore
 	@Test
 	public void testAddTooManyWishes() {
 		try {
@@ -105,4 +115,98 @@ public class RemoteBenchmark {
 		}
 	}
 	
+	@Test
+	public void testBenchmarkAddingOneWish() {
+		List<Long> times = new LinkedList<Long>();
+		
+		String listStr = addList("/repo1");
+		
+		for(int i = 0; i < 1000; i++) {
+			if(i % 100 == 0) {
+				System.out.println("Adding " + i + "th wish.");
+			}
+			
+			long time = System.currentTimeMillis();
+			assertTrue(HttpUtils.makeGetRequest(this.absoluteUrl + listStr + "/add?wishes=1"));
+			time = System.currentTimeMillis() - time;
+			
+			times.add(time);
+			
+			assertTrue(HttpUtils.makeGetRequest(this.absoluteUrl + listStr + "/clear"));
+		}
+		
+		double avgTime = 0l;
+		for(long time : times) {
+			avgTime += time;
+		}
+		
+		avgTime = avgTime / 1000;
+		
+		System.out.println("Average time (in ms) to add one wish: " + avgTime);
+	}
+	
+	@Test
+	public void testBenchmarkDeletingOneWish() {
+		List<Long> times = new LinkedList<Long>();
+		
+		String listStr = addList("/repo1");
+		
+		for(int i = 0; i < 1000; i++) {
+			if(i % 100 == 0) {
+				System.out.println("Deleting " + i + "th wish.");
+			}
+			
+			String wishStr = addWishToEmptyList(this.absoluteUrl + listStr);
+			
+			long time = System.currentTimeMillis();
+			assertTrue(HttpUtils.makeGetRequest(this.absoluteUrl + wishStr + "/delete"));
+			time = System.currentTimeMillis() - time;
+			
+			times.add(time);
+			
+		}
+		
+		double avgTime = 0l;
+		for(long time : times) {
+			avgTime += time;
+		}
+		
+		avgTime = avgTime / 1000;
+		
+		System.out.println("Average time (in ms) to delete one wish: " + avgTime);
+	}
+	
+	// Helper Methods
+	private String addList(String repoIdStr) {
+		String response = HttpUtils.getRequestAsStringResponse(this.absoluteUrl + "/xmas"
+		        + repoIdStr + "/add?lists=1&wishes=0");
+		
+		String[] lines = response.split("\n");
+		
+		String list = null;
+		boolean found = false;
+		for(int i = 0; i < lines.length && !found; i++) {
+			if(lines[i].startsWith("<a href=\"/xmas/")) {
+				String[] contents = lines[i].split("\"");
+				list = contents[1];
+				
+				found = true;
+			}
+		}
+		assertNotNull(list);
+		assert list != null;
+		assertTrue(list.startsWith("/xmas/repo1/"));
+		
+		return list;
+	}
+	
+	private String addWishToEmptyList(String listUrlStr) {
+		assertTrue(HttpUtils.makeGetRequest(listUrlStr + "/add?wishes=1"));
+		
+		String response = HttpUtils.getRequestAsStringResponse(listUrlStr + "?format=urls");
+		
+		String[] lines = response.split("\n");
+		
+		return lines[0];
+	}
 }
