fdeac220def585ee1eddfaa050d6eda327ba123d
Max Völkel <max.voelkel@calpano.com>
2011-09-13 14:17:47 +0000

    Add @Override tags

--- org.xydra.server/src/main/java/org/xydra/server/rest/XAccessExceptionHandler.java
+++ org.xydra.server/src/main/java/org/xydra/server/rest/XAccessExceptionHandler.java
@@ -14,6 +14,7 @@ import org.xydra.store.AccessException;
  **/
 public class XAccessExceptionHandler implements RestlessExceptionHandler {
 	
+	@Override
 	public boolean handleException(Throwable t, IRestlessContext context) {
 		
 		if(t instanceof AccessException) {
