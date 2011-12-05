5835a25f626633efd58fe14f68e0c4900972ad8c
Bjoern.Kaidel@gmail.com <bjoern.kaidel@googlemail.com>
2011-12-05 15:37:05 +0000

    short test for checking whether averages are correctly computed or not

---
+++ org.xydra.csv/src/test/java/org/xydra/csv/GroupByTest.java
@@ -0,0 +1,33 @@
+package org.xydra.csv;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.Test;
+import org.xydra.csv.impl.memory.CsvTable;
+
+
+public class GroupByTest {
+	
+	@Test
+	public void testRightHandDigit() {
+		CsvTable dataTable = new CsvTable();
+		
+		IRow row = dataTable.getOrCreateRow("1", true);
+		row.setValue("X", "0", true);
+		row.setValue("aaa", 5.0, true);
+		
+		IRow row2 = dataTable.getOrCreateRow("2", true);
+		row2.setValue("X", "0", true);
+		row2.setValue("aaa", 8.0, true);
+		
+		CsvTable dataTarget = new CsvTable();
+		TableTools.groupBy(dataTable, Arrays.asList("X"), Collections.EMPTY_LIST,
+		        Arrays.asList("bbb", "aaa"), Collections.EMPTY_LIST, dataTarget);
+		
+		// 8 + 5 = 13, Average should be 6.5
+		assertTrue(Double.parseDouble(dataTarget.getValue("" + 0, "aaa--average")) == 6.5);
+	}
+}
