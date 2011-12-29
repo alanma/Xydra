fa89424c9ef82689e9a6cac0833a47cc99457535
Bjoern.Kaidel@gmail.com <bjoern.kaidel@googlemail.com>
2011-12-29 12:10:35 +0000

    added documentation

--- org.xydra.testgae/src/main/java/org/xydra/testgae/shared/Operations.java
+++ org.xydra.testgae/src/main/java/org/xydra/testgae/shared/Operations.java
@@ -1,5 +1,23 @@
 package org.xydra.testgae.shared;
 
+import org.xydra.testgae.client.RemoteBenchmark;
+
+
+/**
+ * Simple enumeration for the different methods of this application.
+ * 
+ * ADD = Adding a wish
+ * 
+ * DELETE = Deleting a wish
+ * 
+ * EDIT = Editing a wish
+ * 
+ * It's for example being used in {@link RemoteBenchmark}.
+ * 
+ * @author Kaidel
+ * 
+ */
+
 public enum Operations {
 	ADD, DELETE, EDIT
 }
