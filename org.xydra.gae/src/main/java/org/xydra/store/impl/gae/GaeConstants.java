3ccd7754c4108eeed523dd7b4c060fd45a88df6b
Max Völkel <max.voelkel@calpano.com>
2012-01-02 17:20:48 +0000

    Added GaeConstants

---
+++ org.xydra.gae/src/main/java/org/xydra/store/impl/gae/GaeConstants.java
@@ -0,0 +1,8 @@
+package org.xydra.store.impl.gae;
+
+public interface GaeConstants {
+	
+	/** 10 kilobytes, see also QueueConstants.maxTaskSizeBytes(); */
+	public static final long MAX_GAE_TASk_PAYLOAD_SIZE = 10 * 1024;
+	
+}
