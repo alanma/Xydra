24948a4ebfe2dba9922b5f7621eca99ab4a971b4
Max Völkel <max.voelkel@calpano.com>
2012-01-11 12:46:11 +0000

    Added documentation

--- org.xydra.core/src/main/java/org/xydra/base/change/XFieldEvent.java
+++ org.xydra.core/src/main/java/org/xydra/base/change/XFieldEvent.java
@@ -35,6 +35,9 @@ public interface XFieldEvent extends XAtomicEvent {
 	/**
 	 * HOW has the {@link XValue} of the {@link XField} been changed?
 	 * 
+	 * To get the old value, use {@link XModel#getChangeLog()} and get the event
+	 * that has the revision number of {@link #getOldFieldRevision()}.
+	 * 
 	 * @return the new {@link XValue}. null indicates a remove event.
 	 */
 	XValue getNewValue();
