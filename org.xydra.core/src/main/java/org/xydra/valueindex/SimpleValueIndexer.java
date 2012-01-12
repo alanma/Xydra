f884be4e6e5e79b44c6d8398198ab9b8a34ed898
Bjoern.Kaidel@gmail.com <bjoern.kaidel@googlemail.com>
2012-01-12 13:46:12 +0000

    removed TODO, since Regex was correct

--- org.xydra.core/src/main/java/org/xydra/valueindex/SimpleValueIndexer.java
+++ org.xydra.core/src/main/java/org/xydra/valueindex/SimpleValueIndexer.java
@@ -12,9 +12,7 @@ public class SimpleValueIndexer extends StringValueSimpleIndexerAdapter {
 	}
 	
 	public String[] getStringIndexStrings(String value) {
-		// TODO How to deal with punctuation marks etc? (is this the right
-		// regex?)
-		String[] words = value.split("\\W]");
+		String[] words = value.split("[\\W]");
 		String[] indexes = new String[words.length];
 		
 		for(int i = 0; i < indexes.length; i++) {
