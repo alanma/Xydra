29e710d293c482bff59e165deb971b1584b48d10
Bjoern.Kaidel@gmail.com <bjoern.kaidel@googlemail.com>
2012-02-27 11:12:47 +0000

    since our implementation couldn't implement most of IMapSetIndex' methods, this interface no longer extends IMapSetIndex

--- org.xydra.core/src/main/java/org/xydra/valueindex/ValueIndex.java
+++ org.xydra.core/src/main/java/org/xydra/valueindex/ValueIndex.java
@@ -3,12 +3,10 @@ package org.xydra.valueindex;
 import java.util.Iterator;
 
 import org.xydra.base.XAddress;
+import org.xydra.base.rmof.XReadableField;
 import org.xydra.base.value.XValue;
 import org.xydra.core.model.XField;
 import org.xydra.core.model.XModel;
-import org.xydra.core.model.XObject;
-import org.xydra.index.IMapSetIndex;
-import org.xydra.index.query.Constraint;
 import org.xydra.index.query.EqualsConstraint;
 
 
@@ -16,40 +14,15 @@ import org.xydra.index.query.EqualsConstraint;
  * A data structure used for indexing the contents of an {@link XModel}, i.e.
  * the {@link XValue XValues} contained in its {@link XField XFields}.
  * 
- * Even though this interface extends {@link IMapSetIndex}, implementations of
- * this interface do not have to implement all methods of {@link IMapSetIndex}.
- * Please check whether the implementation you are using provides the methods
- * you need.
- * 
  * @author Kaidel
  * 
  */
 
 /*
- * TODO is the "special inheritance" here a good idea? It sounds like it is
- * against the principles of inheritance...
+ * TODO add exception documentation and other missing documentation
  */
 
-public interface ValueIndex extends IMapSetIndex<String,ValueIndexEntry> {
-	
-	/**
-	 * Associates the given {@link ValueIndexEntry} with the given key. The
-	 * counter-variable of the ValueIndexEntry will be ignored, two
-	 * ValueIndexEntry that only differ in the counter (i.e. have the same
-	 * address and value) will be treated as equal entries.
-	 * 
-	 * If there already exists an {@link ValueIndexEntry} with the same address-
-	 * and value-variables as the given entry in the set of entries associated
-	 * with the given key, its counter will be incremented. If no such entry
-	 * exists, the given entry will be added to the set of entries associated
-	 * with the given key with its counter set to 1.
-	 * 
-	 * @param key The key.
-	 * @param entry The ValueIndexEntry which is to be added under the specified
-	 *            key (counter will be ignored)
-	 */
-	@Override
-	public void index(String key, ValueIndexEntry entry);
+public interface ValueIndex {
 	
 	/**
 	 * Associates the given pair of {@link XAddress} and {@link XValue} with the
@@ -62,30 +35,11 @@ public interface ValueIndex extends IMapSetIndex<String,ValueIndexEntry> {
 	 * with the given key with its counter set to 1.
 	 * 
 	 * @param key The key.
-	 * @param objectAddress the address of the {@link XObject} in which the
-	 *            given value exists
+	 * @param fieldAddress the address of the {@link XReadableField} in which
+	 *            the given value exists
 	 * @param value the value which is to be associated with the given key
 	 */
-	public void index(String key, XAddress objectAddress, XValue value);
-	
-	/**
-	 * Tries to remove the given {@link ValueIndexEntry} from the set of entries
-	 * for the given key. The counter-variable of the ValueIndexEntry will be
-	 * ignored, two ValueIndexEntry that only differ in the counter (i.e. have
-	 * the same address and value) will be treated as equal entries.
-	 * 
-	 * If there already exists an {@link ValueIndexEntry} with the same address-
-	 * and value-variables in the set of entries associated with the given key
-	 * as the given entry, it's counter will be decremented. If the counter is
-	 * set to 0 after if was decremented, the entry will be removed from the set
-	 * of entries associated to the given key.
-	 * 
-	 * @param key The key.
-	 * @param entry The ValueIndexEntry which is to be removed (counter will be
-	 *            ignored)
-	 */
-	@Override
-	public void deIndex(String key, ValueIndexEntry entry);
+	public void index(String key, XAddress fieldAddress, XValue value);
 	
 	/**
 	 * Tries to remove the given pair of {@link XAddress} and {@link XValue}
@@ -98,12 +52,12 @@ public interface ValueIndex extends IMapSetIndex<String,ValueIndexEntry> {
 	 * of entries associated to the given key.
 	 * 
 	 * @param key The key.
-	 * @param objectAddress the address of the {@link XObject} in which the
-	 *            given value exists
+	 * @param fieldAddress the address of the {@link XReadableField} in which
+	 *            the given value exists
 	 * @param value the value which is to be removed from the set of values
 	 *            associated with the given key
 	 */
-	public void deIndex(String key, XAddress objectAddress, XValue value);
+	public void deIndex(String key, XAddress fieldAddress, XValue value);
 	
 	/**
 	 * Returns an iterator over all {@link ValueIndexEntry ValueIndexEntries}
@@ -119,10 +73,14 @@ public interface ValueIndex extends IMapSetIndex<String,ValueIndexEntry> {
 	 * @param c1
 	 * @return an iterator that ranges over all entries indexes by keys, where
 	 *         the keys match c1
-	 * @throws UnsupportedOperationException if the given constraint is not an
-	 *             instance of {@link EqualsConstraint}.
 	 */
-	@Override
-	Iterator<ValueIndexEntry> constraintIterator(Constraint<String> c1);
+	Iterator<ValueIndexEntry> constraintIterator(EqualsConstraint<String> c1);
+	
+	public void deIndex(String key);
+	
+	public boolean containsKey(String key);
+	
+	public boolean contains(EqualsConstraint<String> c,
+	        EqualsConstraint<ValueIndexEntry> entryConstraint);
 	
 }
