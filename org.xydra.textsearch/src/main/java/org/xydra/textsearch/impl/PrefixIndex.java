39f6d044ceccde44201b14a353396aa837272a82
Max Völkel <max.voelkel@calpano.com>
2012-03-23 19:44:42 +0000

    Improved pom; made some methods static

--- org.xydra.textsearch/src/main/java/org/xydra/textsearch/impl/PrefixIndex.java
+++ org.xydra.textsearch/src/main/java/org/xydra/textsearch/impl/PrefixIndex.java
@@ -4,37 +4,37 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PrefixIndex<V> extends AbstractTokenIndex<V> {
 
+public class PrefixIndex<V> extends AbstractTokenIndex<V> {
+	
 	private int minPrefixLength;
 	private int maxPrefixLength;
-
+	
 	public PrefixIndex(int minPrefixLength, int maxPrefixLength) {
 		super();
 		this.minPrefixLength = minPrefixLength;
 		this.maxPrefixLength = maxPrefixLength;
 	}
-
+	
 	@Override
 	protected Collection<String> generateTokenFragments(String token) {
-		return generatePrefixes(token, this.minPrefixLength,
-				this.maxPrefixLength);
+		return generatePrefixes(token, this.minPrefixLength, this.maxPrefixLength);
 	}
-
-	private List<String> generatePrefixes(String t, int minimalPrefixLength,
-			int maximalPrefixLength) {
+	
+	private static List<String> generatePrefixes(String t, int minimalPrefixLength,
+	        int maximalPrefixLength) {
 		List<String> result = new LinkedList<String>();
-		if (t.length() < minimalPrefixLength) {
+		if(t.length() < minimalPrefixLength) {
 			return result;
 		} else {
 			int maxPrefixLen = Math.min(t.length(), maximalPrefixLength);
-
-			for (int i = minimalPrefixLength; i <= maxPrefixLen; i++) {
+			
+			for(int i = minimalPrefixLength; i <= maxPrefixLen; i++) {
 				String subString = t.substring(0, i);
 				result.add(subString);
 			}
 			return result;
 		}
 	}
-
+	
 }
