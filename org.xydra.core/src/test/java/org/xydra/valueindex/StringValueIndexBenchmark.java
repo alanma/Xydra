05d9309d358ef6de9ec5c8eda0aee3672dc9a35c
Bjoern.Kaidel@gmail.com <bjoern.kaidel@googlemail.com>
2012-04-12 09:09:00 +0000

    continued writing benchmark

--- org.xydra.core/src/test/java/org/xydra/valueindex/StringValueIndexBenchmark.java
+++ org.xydra.core/src/test/java/org/xydra/valueindex/StringValueIndexBenchmark.java
@@ -1,11 +1,11 @@
 package org.xydra.valueindex;
 
-import static org.junit.Assert.assertTrue;
-
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -15,7 +15,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.xydra.base.XID;
 import org.xydra.base.XX;
+import org.xydra.core.model.XField;
 import org.xydra.core.model.XModel;
+import org.xydra.core.model.XObject;
 import org.xydra.core.serialize.SerializedModel;
 import org.xydra.core.serialize.XydraElement;
 import org.xydra.core.serialize.xml.XmlParser;
@@ -23,6 +25,8 @@ import org.xydra.core.serialize.xml.XmlParser;
 
 public class StringValueIndexBenchmark {
 	private List<XModel> models;
+	final static String lineSeparator = System.getProperty("line.separator");
+	final static String testDataPath = "../TestData/anonymized-emails/";
 	
 	@Before
 	public void setup() {
@@ -56,7 +60,7 @@ public class StringValueIndexBenchmark {
 		this.models = new ArrayList<XModel>();
 		XID actorId = XX.createUniqueId();
 		
-		File f = new File("../TestData/anonymized-emails/");
+		File f = new File(testDataPath);
 		File[] files = f.listFiles();
 		
 		for(int i = 0; i < files.length; i++) {
@@ -87,11 +91,14 @@ public class StringValueIndexBenchmark {
 		}
 	}
 	
+	@SuppressWarnings("unused")
 	@Test
 	public void benchmarkModelIndexing() {
 		HashSet<XID> emptySet = new HashSet<XID>();
 		
 		for(XModel model : this.models) {
+			writeDescription(model);
+			
 			StringMap map = new MockStringMap();
 			StringValueIndex index = new StringValueIndex(map);
 			
@@ -103,11 +110,62 @@ public class StringValueIndexBenchmark {
 			long end = System.currentTimeMillis();
 			
 			System.out.println(end - start);
+			
+			writeData(model, end - start);
 		}
 	}
 	
-	@Test
-	public void test() {
-		assertTrue(true);
+	private void writeDescription(XModel model) {
+		String idString = model.getId().toString();
+		try {
+			File f = new File(testDataPath + idString + ".txt");
+			if(f.exists()) {
+				return;
+			}
+			int objects = 0;
+			int fields = 0;
+			int values = 0;
+			
+			for(XID objectId : model) {
+				objects++;
+				XObject object = model.getObject(objectId);
+				
+				for(XID fieldId : object) {
+					fields++;
+					XField field = object.getField(fieldId);
+					if(field.getValue() != null) {
+						values++;
+					}
+				}
+			}
+			
+			BufferedWriter out = new BufferedWriter(new FileWriter(f, true));
+			out.write("#Objects:, " + objects);
+			out.write(lineSeparator);
+			out.write("#Fields:, " + fields);
+			out.write(lineSeparator);
+			out.write("#Non-Null Values:, " + values);
+			out.write(lineSeparator);
+			
+			out.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void writeData(XModel model, long data) {
+		String idString = model.getId().toString();
+		try {
+			File f = new File(testDataPath + idString + ".txt");
+			assert f.exists();
+			
+			BufferedWriter out = new BufferedWriter(new FileWriter(f, true));
+			out.write("" + data);
+			out.write(lineSeparator);
+			
+			out.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
