a1a08cd0aa4b646842d7bf545118428847bceae4
Bjoern.Kaidel@gmail.com <bjoern.kaidel@googlemail.com>
2012-06-20 17:18:59 +0000

    added a FIXME

--- org.xydra.gae/src/test/java/org/xydra/store/impl/gae/GaeDetailedPersistenceTest.java
+++ org.xydra.gae/src/test/java/org/xydra/store/impl/gae/GaeDetailedPersistenceTest.java
@@ -11,6 +11,22 @@ import org.xydra.store.AbstractPersistenceTest;
 
 public class GaeDetailedPersistenceTest extends AbstractPersistenceTest {
 	
+	/*
+	 * FIXME Running the complete test on my machine sometimes results in an IO
+	 * Exception ("IO Allocation Error" or something like that) of Eclipse (not
+	 * of Java), which crashes Eclipse. According to the Eclipse readme this
+	 * happens when the Java heap space is full and can be fixed by making the
+	 * heap space bigger through an Eclipse parameter - but this doesn't seem to
+	 * be the right way to go, using Xydra as I do in the test shouldn't result
+	 * in allocating the complete heap space, I think.
+	 * 
+	 * I unfortunately don't know the reason. Are the XModel-Java Objects too
+	 * big or is the output on the console from the logger "too much"? Running
+	 * the MemoryPersistenceTest of org.xydra.core never shows this behavior.
+	 * 
+	 * ~Kaidel
+	 */
+	
 	private static final Logger log = LoggerFactory.getLogger(GaeDetailedPersistenceTest.class);
 	
 	@BeforeClass
