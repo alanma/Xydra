3a86aabcd9f9c5fa202c23b32a0f6d505f04dddd
Max Völkel <max.voelkel@calpano.com>
2012-06-27 17:25:43 +0000

    Made SonicListener interface simpler

--- com.sonicmetrics.core/src/main/java/com/sonicmetrics/core/shared/ISonicDB.java
+++ com.sonicmetrics.core/src/main/java/com/sonicmetrics/core/shared/ISonicDB.java
@@ -35,4 +35,20 @@ public interface ISonicDB extends ISonicListener {
 	 */
 	long getCurrentTime();
 	
+	/**
+	 * Set a key string as it is used in the backend.
+	 * 
+	 * @param sonicEvent without a key
+	 */
+	void setKey(ISonicEvent sonicEvent);
+	
+	/**
+	 * Method returns before event is persisted/sent.
+	 * 
+	 * @param sonicEvent to be received, may have a key defined which was
+	 *            obtained via {@link #setKey(ISonicEvent)} in the same
+	 *            implementation
+	 */
+	void receiveEventAsync(ISonicEvent sonicEvent);
+	
 }
--- com.sonicmetrics.core/src/main/java/com/sonicmetrics/core/shared/query/ISonicListener.java
+++ com.sonicmetrics.core/src/main/java/com/sonicmetrics/core/shared/query/ISonicListener.java
@@ -11,26 +11,8 @@ import com.sonicmetrics.core.shared.ISonicEvent;
 public interface ISonicListener {
 	
 	/**
-	 * @param sonicEvent to be received, may have a key defined which was
-	 *            obtained via {@link #setKey(ISonicEvent)} in the same
-	 *            implementation
+	 * @param sonicEvent to be received,
 	 */
 	void receiveEvent(ISonicEvent sonicEvent);
-	
-	/**
-	 * Method returns before event is persisted/sent.
-	 * 
-	 * @param sonicEvent to be received, may have a key defined which was
-	 *            obtained via {@link #setKey(ISonicEvent)} in the same
-	 *            implementation
-	 */
-	void receiveEventAsync(ISonicEvent sonicEvent);
-	
-	/**
-	 * Set a key string as it is used in the backend.
-	 * 
-	 * @param sonicEvent without a key
-	 */
-	void setKey(ISonicEvent sonicEvent);
 	
 }
