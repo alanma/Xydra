896f03ce439105f9ea16d3d4e1afd4184807ded0
Max Völkel <max.voelkel@calpano.com>
2012-07-24 13:07:05 +0000

    added two new annotations

---
+++ org.xydra.annotations/src/main/java/org/xydra/annotations/ThreadSafe.java
@@ -0,0 +1,21 @@
+package org.xydra.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * The annotated method or class can be accessed by several threads at the same
+ * time without causing errors.
+ * 
+ * @author xamde
+ */
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target({ ElementType.TYPE, ElementType.METHOD })
+public @interface ThreadSafe {
+	
+}
