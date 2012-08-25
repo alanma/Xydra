b45863c38d9035978252180c4e00bcb29cc04151
Max Völkel <max.voelkel@calpano.com>
2012-08-25 08:19:57 +0000

    Fix encoding of all kinds of line-breaks in json strings

---
+++ org.xydra.core/src/main/java/org/xydra/core/serialize/LineBreaks.java
@@ -0,0 +1,70 @@
+package org.xydra.core.serialize;
+
+/**
+ * Handles line-break problems across platforms.
+ * 
+ * Tags for fulltext search: newline, linebreak,
+ * 
+ * Sources: http://en.wikipedia.org/wiki/Newline
+ * 
+ * Common usages:
+ * 
+ * Unix, Mac: LF
+ * 
+ * Windows, HTTP: CR LF
+ * 
+ * Old Mac: CR
+ * 
+ * @author xamde
+ */
+public class LineBreaks {
+	
+	/*
+	 * The Unicode standard defines a large number of Stringacters that
+	 * conforming applications should recognize as line terminators
+	 */
+	
+	/** Line Feed, U+000A = \n */
+	public static final String LF = "\n";
+	
+	/** Vertical Tab, U+000B */
+	public static final String VT = "\u000B";
+	
+	/** Form Feed, U+000C */
+	public static final String FF = "\u000C";
+	
+	/** Carriage Return, U+000D = \r */
+	public static final String CR = "\r";
+	
+	/** CR (U+000D) followed by LF (U+000A) */
+	public static final String CRLF = CR + LF;
+	
+	/** Next Line, U+0085 */
+	public static final String NEL = "\u0085";
+	
+	/** Line Separator, U+2028 */
+	public static final String LS = "\u2028";
+	
+	/** Paragraph Separator, U+2029 */
+	public static final String PS = "\u2029";
+	
+	/**
+	 * @param in
+	 * @return a string in which all line breaks have been replaced with the
+	 *         standard java line break \n
+	 */
+	public static String normalizeLinebreaks(String in) {
+		String result = in;
+		result = result.replace(LF, "\n");
+		result = result.replace(VT, "\n");
+		result = result.replace(FF, "\n");
+		// important: first CRLF, then CR
+		result = result.replace(CRLF, "\n");
+		result = result.replace(CR, "\n");
+		result = result.replace(NEL, "\n");
+		result = result.replace(LS, "\n");
+		result = result.replace(PS, "\n");
+		return result;
+	}
+	
+}
--- org.xydra.core/src/main/java/org/xydra/core/serialize/json/JsonEncoder.java
+++ org.xydra.core/src/main/java/org/xydra/core/serialize/json/JsonEncoder.java
@@ -6,23 +6,24 @@ package org.xydra.core.serialize.json;
 import org.xydra.annotations.RequiresAppEngine;
 import org.xydra.annotations.RunsInAppEngine;
 import org.xydra.annotations.RunsInGWT;
+import org.xydra.core.serialize.LineBreaks;
 
 
+/**
+ * Encode string values as valid JSON
+ * 
+ * @author xamde
+ */
 @RunsInGWT(true)
 @RunsInAppEngine(true)
 @RequiresAppEngine(false)
 public class JsonEncoder {
 	
 	public static String encode(String in) {
-		String result = in;
+		String result = LineBreaks.normalizeLinebreaks(in);
 		result = result.replace("\\", "\\\\");
 		result = result.replace("\"", "\\\"");
-		// windows
-		result = result.replace("\r\n", "\\n");
-		// more or less standard
 		result = result.replace("\n", "\\n");
-		// old macs
-		result = result.replace("\r", "\\n");
 		return result;
 	}
 	
