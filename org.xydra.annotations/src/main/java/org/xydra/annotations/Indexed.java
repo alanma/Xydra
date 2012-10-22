bdbf9150bcc2ef9b080fc4d76f08f528be2b4f97
Max Völkel <max.voelkel@calpano.com>
2012-10-22 17:27:12 +0000

    Add "Indexed" annotation

---
+++ org.xydra.annotations/src/main/java/org/xydra/annotations/Indexed.java
@@ -0,0 +1,17 @@
+package org.xydra.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * Documentation: If this annotation is present, the field is indexed. String
+ * denotes where. E.g. "server".
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target({ ElementType.FIELD })
+public @interface Indexed {
+	String[] value();
+}
