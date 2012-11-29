8feb6c72aab09abf78cbeb905f62aed0982a0f5c
Max Völkel <max.voelkel@calpano.com>
2012-11-29 19:56:51 +0000

    Docu; compute auth also when there is no authentication db

--- org.xydra.core/src/main/java/org/xydra/base/change/XCommandUtils.java
+++ org.xydra.core/src/main/java/org/xydra/base/change/XCommandUtils.java
@@ -10,30 +10,30 @@ import org.xydra.annotations.RunsInGWT;
  */
 @RunsInGWT(true)
 public class XCommandUtils {
-	
-	public static boolean changedSomething(long commandResult) {
-		return commandResult >= 0;
-	}
-	
-	/**
-	 * @param commandResult
-	 * @return true if there was no error. No change is considered success.
-	 */
-	public static boolean success(long commandResult) {
-		return commandResult >= 0 || commandResult == XCommand.NOCHANGE;
-	}
-	
-	/**
-	 * @param result
-	 * @return true if the result indicates a failed Xydra command. Nothing
-	 *         else.
-	 */
-	public static boolean failed(long result) {
-		return result == XCommand.FAILED;
-	}
-	
-	public static boolean noChange(long result) {
-		return result == XCommand.NOCHANGE;
-	}
-	
+    
+    public static boolean changedSomething(long commandResult) {
+        return commandResult >= 0;
+    }
+    
+    /**
+     * @param commandResult
+     * @return true if there was no error. NoChange is also a success.
+     */
+    public static boolean success(long commandResult) {
+        return commandResult >= 0 || commandResult == XCommand.NOCHANGE;
+    }
+    
+    /**
+     * @param result
+     * @return true if the result indicates a failed Xydra command. Nothing
+     *         else.
+     */
+    public static boolean failed(long result) {
+        return result == XCommand.FAILED;
+    }
+    
+    public static boolean noChange(long result) {
+        return result == XCommand.NOCHANGE;
+    }
+    
 }
