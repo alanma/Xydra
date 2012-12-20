6229a3f75c056c34629c7d0d27b3d72cbaa1ed39
Max Völkel <max.voelkel@calpano.com>
2012-12-20 14:38:16 +0000

    preparing editor integration

--- org.xydra.webadmin/src/main/java/org/xydra/webadmin/gwt/client/widgets/FieldWidget.java
+++ org.xydra.webadmin/src/main/java/org/xydra/webadmin/gwt/client/widgets/FieldWidget.java
@@ -13,32 +13,45 @@ import com.google.gwt.user.client.ui.Widget;
 
 
 public class FieldWidget extends Composite {
-	
-	interface ViewUiBinder extends UiBinder<Widget,FieldWidget> {
-	}
-	
-	private static ViewUiBinder uiBinder = GWT.create(ViewUiBinder.class);
-	
-	@SuppressWarnings("unused")
-	private XID fieldID;
-	@SuppressWarnings("unused")
-	private Object value;
-	
-	@UiField
-	FlowPanel fieldPanel;
-	@UiField
-	Label fieldLabel;
-	@UiField
-	Anchor valueAnchor;
-	
-	public FieldWidget(XID fieldID, Object value) {
-		
-		initWidget(uiBinder.createAndBindUi(this));
-		
-		this.fieldID = fieldID;
-		this.value = value;
-		
-		this.fieldLabel.setText(fieldID.toString());
-		this.valueAnchor.setText(" : " + value);
-	}
+    
+    interface ViewUiBinder extends UiBinder<Widget,FieldWidget> {
+    }
+    
+    private static ViewUiBinder uiBinder = GWT.create(ViewUiBinder.class);
+    
+    @SuppressWarnings("unused")
+    private XID fieldID;
+    @SuppressWarnings("unused")
+    private Object value;
+    
+    @UiField
+    FlowPanel fieldPanel;
+    @UiField
+    Label fieldLabel;
+    @UiField
+    Anchor valueAnchor;
+    
+    public FieldWidget(XID fieldID, Object value) {
+        
+        initWidget(uiBinder.createAndBindUi(this));
+        
+        this.fieldID = fieldID;
+        this.value = value;
+        
+        this.fieldLabel.setText(fieldID.toString());
+        this.valueAnchor.setText(" : " + value);
+        
+        // TODO clean this up
+//        if(value != null) {
+//            EditListener listener = new EditListener() {
+//                
+//                @Override
+//                public void newValue(XValue value) {
+//                    // TODO send to server or wait while we build a txn
+//                }
+//            };
+//            XValueEditor editor = XValueEditor.get(value, listener);
+//            this.fieldPanel.add(editor);
+//        }
+    }
 }
