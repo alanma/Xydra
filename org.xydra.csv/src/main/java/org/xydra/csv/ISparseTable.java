215ddea9ae4bc4d0b85967fb6f8cc76cc43bf842
Max Völkel <max.voelkel@calpano.com>
2013-02-08 14:10:29 +0000

    Added some convenience methods; Write BOM in files; Switch back to ";" as cell delimiter.

--- org.xydra.csv/src/main/java/org/xydra/csv/ISparseTable.java
+++ org.xydra.csv/src/main/java/org/xydra/csv/ISparseTable.java
@@ -14,221 +14,223 @@ import org.xydra.csv.impl.memory.Row;
  * 
  */
 public interface ISparseTable extends Iterable<Row>, IRowHandler {
-	
-	/**
-	 * Adds a column name which will appear in the output but not necessarily
-	 * use any space if now rows contain entries for it.
-	 * 
-	 * @param columnName
-	 */
-	void addColumnName(String columnName);
-	
-	/**
-	 * Aggregates all rows that share the same values for the given keys.
-	 * 
-	 * Aggregation is simple: If the values can be parsed as numbers, they are
-	 * added. Otherwise string concatenation with a separator is used.
-	 * 
-	 * Example: Aggregating a table with columns "a", "b" and "c" using
-	 * keyColumnNames=("a","b") will add all numbers of "c" where "a" and "b"
-	 * are equal. Given the table
-	 * <table>
-	 * <tr>
-	 * <th>a</th>
-	 * <th>b</th>
-	 * <th>c</th>
-	 * </tr>
-	 * <tr>
-	 * <td>blue</td>
-	 * <td>on</td>
-	 * <td>1</td>
-	 * </tr>
-	 * <tr>
-	 * <td>red</td>
-	 * <td>on</td>
-	 * <td>3</td>
-	 * </tr>
-	 * <tr>
-	 * <td>blue</td>
-	 * <td>off</td>
-	 * <td>7</td>
-	 * </tr>
-	 * <tr>
-	 * <td>red</td>
-	 * <td>on</td>
-	 * <td>11</td>
-	 * </tr>
-	 * <tr>
-	 * <td>green</td>
-	 * <td>on</td>
-	 * <td>13</td>
-	 * </tr>
-	 * </table>
-	 * results in
-	 * <table>
-	 * <tr>
-	 * <th>a</th>
-	 * <th>b</th>
-	 * <th>c</th>
-	 * </tr>
-	 * <tr>
-	 * <td>blue</td>
-	 * <td>on</td>
-	 * <td>1</td>
-	 * </tr>
-	 * <tr>
-	 * <td>blue</td>
-	 * <td>off</td>
-	 * <td>7</td>
-	 * </tr>
-	 * <tr>
-	 * <td>red</td>
-	 * <td>on</td>
-	 * <td>14</td>
-	 * </tr>
-	 * <tr>
-	 * <td>green</td>
-	 * <td>on</td>
-	 * <td>13</td>
-	 * </tr>
-	 * </table>
-	 * 
-	 * Existing sorting order of table is ignored.
-	 * 
-	 * @param keyColumnNames an array of column names which is used for
-	 *            aggregation.
-	 */
-	void aggregate(String[] keyColumnNames);
-	
-	/**
-	 * Deletes all data, but remembers the column names. This allows to generate
-	 * several tables with the same layout, even if data is cleared in between.
-	 */
-	void clear();
-	
-	/**
-	 * Create a sub-table
-	 * 
-	 * @param columnName to be filtered
-	 * @param value may be null
-	 * @return a sub-table with dropped rows. The resulting table has no row in
-	 *         which column 'columnName' has the given 'value'.
-	 */
-	ISparseTable drop(String columnName, String value);
-	
-	/**
-	 * Create a sub-table
-	 * 
-	 * @param columnName never null
-	 * @param value may be null
-	 * @return a sub-table where in each row columnName has the given value
-	 */
-	ISparseTable filter(String columnName, String value);
-	
-	Set<String> getColumnNames();
-	
-	/**
-	 * @return sorted column names or null if column names are not sorted (see
-	 *         constructor)
-	 */
-	Iterable<String> getColumnNamesSorted();
-	
-	/**
-	 * Returns an existing row or creates a new one. The new row is guaranteed
-	 * to be inserted into the table.
-	 * 
-	 * @param rowName a String that uniquely identifies a {@link Row}. Never
-	 *            null.
-	 * @param create if true, creates a new {@link Row} if there is none yet. If
-	 *            false and there was now row, null is returned.
-	 * @return a new or exiting {@link Row} or null (if 'create' was false and
-	 *         no {@link Row} with given 'rowName' exists).
-	 */
-	IRow getOrCreateRow(String rowName, boolean create);
-	
-	boolean getParamAggregateStrings();
-	
-	boolean getParamRestrictToExcelSize();
-	
-	/**
-	 * @param row never null
-	 * @param column never null
-	 * @return the table value at cell ('row','column')
-	 */
-	String getValue(String row, String column);
-	
-	/**
-	 * Increments a value at (row,column). Creates the cell before, if
-	 * necessary.
-	 * 
-	 * @param row never null
-	 * @param column never null
-	 * @param increment the increment, often '1' is used.
-	 * @throws WrongDatatypeException if the stored value cannot be cast to a
-	 *             long.
-	 */
-	void incrementValue(String row, String column, int increment) throws WrongDatatypeException;
-	
-	/**
-	 * See also {@link Iterable#iterator()}.
-	 * 
-	 * Some implementations return elements sorted by row key.
-	 */
-	@Override
-	Iterator<Row> iterator();
-	
-	/**
-	 * Removes all rows that match the given RowFilter.
-	 * 
-	 * @param rowFilter never null
-	 */
-	void removeRowsMatching(RowFilter rowFilter);
-	
-	/**
-	 * @return the number of rows in the table. A very fast operation.
-	 */
-	int rowCount();
-	
-	/**
-	 * @param aggregateStrings Default is true. If false, strings are not
-	 *            aggregated.
-	 */
-	void setParamAggregateStrings(boolean aggregateStrings);
-	
-	/**
-	 * @param b if true, the table restricts itself to a size Excel can handle
-	 *            and ignores too much data. Warnings are logged in this case.
-	 * 
-	 *            Default is false.
-	 */
-	void setParamRestrictToExcelSize(boolean b);
-	
-	/**
-	 * Set an initial value to cell (rowName, columnName)
-	 * 
-	 * @param rowName never null
-	 * @param columnName never null
-	 * @param value may be null
-	 * @throws IllegalStateException if there was already a value
-	 */
-	void setValueInitial(String rowName, String columnName, String value);
-	
-	/**
-	 * For every change in 'colName' a new sub-table is created with the rows
-	 * having the same values for 'colName'. Sorting is always performed
-	 * automatically before executing this command.
-	 * 
-	 * @param colName never null
-	 * @return a Map from String (the value of column 'colName') to
-	 *         {@link ISparseTable}
-	 */
-	Map<String,? extends ISparseTable> split(String colName);
-	
-	/**
-	 * Visit all rows with an {@link IRowVisitor}.
-	 * 
-	 * @param rowVisitor never null
-	 */
-	void visitRows(IRowVisitor rowVisitor);
-	
+    
+    /**
+     * Adds a column name which will appear in the output but not necessarily
+     * use any space if now rows contain entries for it.
+     * 
+     * @param columnName
+     */
+    void addColumnName(String columnName);
+    
+    /**
+     * Aggregates all rows that share the same values for the given keys.
+     * 
+     * Aggregation is simple: If the values can be parsed as numbers, they are
+     * added. Otherwise string concatenation with a separator is used.
+     * 
+     * Example: Aggregating a table with columns "a", "b" and "c" using
+     * keyColumnNames=("a","b") will add all numbers of "c" where "a" and "b"
+     * are equal. Given the table
+     * <table>
+     * <tr>
+     * <th>a</th>
+     * <th>b</th>
+     * <th>c</th>
+     * </tr>
+     * <tr>
+     * <td>blue</td>
+     * <td>on</td>
+     * <td>1</td>
+     * </tr>
+     * <tr>
+     * <td>red</td>
+     * <td>on</td>
+     * <td>3</td>
+     * </tr>
+     * <tr>
+     * <td>blue</td>
+     * <td>off</td>
+     * <td>7</td>
+     * </tr>
+     * <tr>
+     * <td>red</td>
+     * <td>on</td>
+     * <td>11</td>
+     * </tr>
+     * <tr>
+     * <td>green</td>
+     * <td>on</td>
+     * <td>13</td>
+     * </tr>
+     * </table>
+     * results in
+     * <table>
+     * <tr>
+     * <th>a</th>
+     * <th>b</th>
+     * <th>c</th>
+     * </tr>
+     * <tr>
+     * <td>blue</td>
+     * <td>on</td>
+     * <td>1</td>
+     * </tr>
+     * <tr>
+     * <td>blue</td>
+     * <td>off</td>
+     * <td>7</td>
+     * </tr>
+     * <tr>
+     * <td>red</td>
+     * <td>on</td>
+     * <td>14</td>
+     * </tr>
+     * <tr>
+     * <td>green</td>
+     * <td>on</td>
+     * <td>13</td>
+     * </tr>
+     * </table>
+     * 
+     * Existing sorting order of table is ignored.
+     * 
+     * @param keyColumnNames an array of column names which is used for
+     *            aggregation.
+     */
+    void aggregate(String[] keyColumnNames);
+    
+    /**
+     * Deletes all data, but remembers the column names. This allows to generate
+     * several tables with the same layout, even if data is cleared in between.
+     */
+    void clear();
+    
+    /**
+     * Create a sub-table
+     * 
+     * @param columnName to be filtered
+     * @param value may be null
+     * @return a sub-table with dropped rows. The resulting table has no row in
+     *         which column 'columnName' has the given 'value'.
+     */
+    ISparseTable drop(String columnName, String value);
+    
+    /**
+     * Create a sub-table
+     * 
+     * @param columnName never null
+     * @param value may be null
+     * @return a sub-table where in each row columnName has the given value
+     */
+    ISparseTable filter(String columnName, String value);
+    
+    Set<String> getColumnNames();
+    
+    /**
+     * @return sorted column names or null if column names are not sorted (see
+     *         constructor)
+     */
+    Iterable<String> getColumnNamesSorted();
+    
+    /**
+     * Returns an existing row or creates a new one. The new row is guaranteed
+     * to be inserted into the table.
+     * 
+     * @param rowName a String that uniquely identifies a {@link Row}. Never
+     *            null.
+     * @param create if true, creates a new {@link Row} if there is none yet. If
+     *            false and there was now row, null is returned.
+     * @return a new or exiting {@link Row} or null (if 'create' was false and
+     *         no {@link Row} with given 'rowName' exists).
+     */
+    IRow getOrCreateRow(String rowName, boolean create);
+    
+    boolean getParamAggregateStrings();
+    
+    boolean getParamRestrictToExcelSize();
+    
+    /**
+     * @param row never null
+     * @param column never null
+     * @return the table value at cell ('row','column')
+     */
+    String getValue(String row, String column);
+    
+    /**
+     * Increments a value at (row,column). Creates the cell before, if
+     * necessary.
+     * 
+     * @param row never null
+     * @param column never null
+     * @param increment the increment, often '1' is used.
+     * @throws WrongDatatypeException if the stored value cannot be cast to a
+     *             long.
+     */
+    void incrementValue(String row, String column, int increment) throws WrongDatatypeException;
+    
+    /**
+     * See also {@link Iterable#iterator()}.
+     * 
+     * Some implementations return elements sorted by row key.
+     */
+    @Override
+    Iterator<Row> iterator();
+    
+    /**
+     * Removes all rows that match the given RowFilter.
+     * 
+     * @param rowFilter never null
+     */
+    void removeRowsMatching(RowFilter rowFilter);
+    
+    /**
+     * @return the number of rows in the table. A very fast operation.
+     */
+    int rowCount();
+    
+    /**
+     * @param aggregateStrings Default is true. If false, strings are not
+     *            aggregated.
+     */
+    void setParamAggregateStrings(boolean aggregateStrings);
+    
+    /**
+     * @param b if true, the table restricts itself to a size Excel can handle
+     *            and ignores too much data. Warnings are logged in this case.
+     * 
+     *            Default is false.
+     */
+    void setParamRestrictToExcelSize(boolean b);
+    
+    /**
+     * Set an initial value to cell (rowName, columnName)
+     * 
+     * @param rowName never null
+     * @param columnName never null
+     * @param value may be null
+     * @throws IllegalStateException if there was already a value
+     */
+    void setValueInitial(String rowName, String columnName, String value);
+    
+    void setValueInitial(String rowName, String columnName, long value);
+    
+    /**
+     * For every change in 'colName' a new sub-table is created with the rows
+     * having the same values for 'colName'. Sorting is always performed
+     * automatically before executing this command.
+     * 
+     * @param colName never null
+     * @return a Map from String (the value of column 'colName') to
+     *         {@link ISparseTable}
+     */
+    Map<String,? extends ISparseTable> split(String colName);
+    
+    /**
+     * Visit all rows with an {@link IRowVisitor}.
+     * 
+     * @param rowVisitor never null
+     */
+    void visitRows(IRowVisitor rowVisitor);
+    
 }
--- org.xydra.csv/src/main/java/org/xydra/csv/impl/memory/CsvCodec.java
+++ org.xydra.csv/src/main/java/org/xydra/csv/impl/memory/CsvCodec.java
@@ -11,144 +11,144 @@ import java.util.List;
  *         Runs in GWT
  */
 public class CsvCodec {
-	
-	public static final byte[] BOM_UTF8 = new byte[] { (byte)0xEF, (byte)0xBB, (byte)0xBF };
-	
-	public static final byte[] BOM_UTF16LE = new byte[] { (byte)0xFF, (byte)0xFE, (byte)0x00,
-	        (byte)0x00 };
-	
-	public static final Object CELL_DELIMITER = "\t";
-	
-	/**
-	 * Decode a single cell
-	 * 
-	 * @param encoded
-	 * @return the pure value string
-	 */
-	public static String excelDecode(String encoded) {
-		if(encoded.equals("") || encoded.equals("\"\"") || encoded.equals("=\"\"")) {
-			return null;
-		}
-		
-		String decoded = encoded;
-		/* Excel formula mode */
-		if(decoded.startsWith("=")) {
-			decoded = decoded.substring(1);
-		}
-		if(decoded.startsWith("\"") && decoded.endsWith("\"")) {
-			decoded = decoded.substring(1, decoded.length() - 1);
-		}
-		// unescape
-		decoded = decoded.replace("\"\"", "\"");
-		
-		return decoded;
-	}
-	
-	/**
-	 * Use safe CSV encoding, that is, surround by " and escape all quotes.
-	 * 
-	 * Enclosing all fields in quotes is not strictly required, but lets Excel
-	 * open an CSV file with default settings without mangling e.g. dates.
-	 * 
-	 * @param value to be encoded
-	 * @return a string that excel accepts
-	 */
-	public static String excelEncode(String value) {
-		if(value == null || value.equals("null")) {
-			return "\"\"";
-		}
-		
-		String escaped;
-		if(value.contains("\"")) {
-			escaped = value.replace("\"", "\"\"");
-		} else {
-			escaped = value;
-		}
-		
-		// proprietary multi-line-handling
-		escaped = escaped.replace("\n", "§N");
-		escaped = escaped.replace("\r", "§R");
-		
-		/* Adding the '=' sign makes it more robust to open on Mac Office */
-		return "=\"" + escaped + "\"";
-	}
-	
-	public static String[] splitAtUnquotedSemicolon(String line) {
-		boolean inQuote = false;
-		int index = 0;
-		List<String> result = new LinkedList<String>();
-		StringBuffer currentString = new StringBuffer();
-		while(index < line.length()) {
-			char c = line.charAt(index);
-			switch(c) {
-			case '\"': {
-				if(inQuote) {
-					inQuote = false;
-				} else {
-					inQuote = true;
-				}
-				currentString.append(c);
-			}
-				break;
-			case ';':
-			case '\t': {
-				if(inQuote) {
-					// just copy over
-					currentString.append(c);
-				} else {
-					// terminate current string
-					result.add(currentString.toString());
-					currentString = new StringBuffer();
-				}
-			}
-				break;
-			default: {
-				// just copy over
-				currentString.append(c);
-			}
-				break;
-			}
-			index++;
-		}
-		result.add(currentString.toString());
-		return result.toArray(new String[result.size()]);
-	}
-	
-	/**
-	 * @param line can be null
-	 * @return the given line as excel-decoded list of Strings
-	 */
-	public static List<String> excelDecodeRow(String line) {
-		if(line == null) {
-			return Collections.emptyList();
-		}
-		assert line != null;
-		List<String> row = new LinkedList<String>();
-		String[] datas = CsvCodec.splitAtUnquotedSemicolon(line);
-		for(int i = 0; i < datas.length; i++) {
-			String value = CsvCodec.excelDecode(datas[i]);
-			row.add(value);
-		}
-		return row;
-	}
-	
-	public static String excelEncodeRow(List<String> values) {
-		if(values == null) {
-			return "";
-		}
-		assert values != null;
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < values.size(); i++) {
-			sb.append(CsvCodec.excelEncode(values.get(i)));
-			if(i + 1 < values.size()) {
-				sb.append(CELL_DELIMITER);
-			}
-		}
-		return sb.toString();
-	}
-	
-	// public static void main(String[] args) {
-	// System.out.println(excelDecodeRow(excelEncodeRow(Arrays.asList("\"a\"",
-	// null, "c"))));
-	// }
+    
+    public static final byte[] BOM_UTF8 = new byte[] { (byte)0xEF, (byte)0xBB, (byte)0xBF };
+    
+    public static final byte[] BOM_UTF16LE = new byte[] { (byte)0xFF, (byte)0xFE, (byte)0x00,
+            (byte)0x00 };
+    
+    public static final String CELL_DELIMITER = ";";
+    
+    /**
+     * Decode a single cell
+     * 
+     * @param encoded
+     * @return the pure value string
+     */
+    public static String excelDecode(String encoded) {
+        if(encoded.equals("") || encoded.equals("\"\"") || encoded.equals("=\"\"")) {
+            return null;
+        }
+        
+        String decoded = encoded;
+        /* Excel formula mode */
+        if(decoded.startsWith("=")) {
+            decoded = decoded.substring(1);
+        }
+        if(decoded.startsWith("\"") && decoded.endsWith("\"")) {
+            decoded = decoded.substring(1, decoded.length() - 1);
+        }
+        // unescape
+        decoded = decoded.replace("\"\"", "\"");
+        
+        return decoded;
+    }
+    
+    /**
+     * Use safe CSV encoding, that is, surround by " and escape all quotes.
+     * 
+     * Enclosing all fields in quotes is not strictly required, but lets Excel
+     * open an CSV file with default settings without mangling e.g. dates.
+     * 
+     * @param value to be encoded
+     * @return a string that excel accepts
+     */
+    public static String excelEncode(String value) {
+        if(value == null || value.equals("null")) {
+            return "\"\"";
+        }
+        
+        String escaped;
+        if(value.contains("\"")) {
+            escaped = value.replace("\"", "\"\"");
+        } else {
+            escaped = value;
+        }
+        
+        // proprietary multi-line-handling
+        escaped = escaped.replace("\n", "§N");
+        escaped = escaped.replace("\r", "§R");
+        
+        /* Adding the '=' sign makes it more robust to open on Mac Office */
+        return "\"" + escaped + "\"";
+    }
+    
+    public static String[] splitAtUnquotedSemicolon(String line) {
+        boolean inQuote = false;
+        int index = 0;
+        List<String> result = new LinkedList<String>();
+        StringBuffer currentString = new StringBuffer();
+        while(index < line.length()) {
+            char c = line.charAt(index);
+            switch(c) {
+            case '\"': {
+                if(inQuote) {
+                    inQuote = false;
+                } else {
+                    inQuote = true;
+                }
+                currentString.append(c);
+            }
+                break;
+            case ';':
+            case '\t': {
+                if(inQuote) {
+                    // just copy over
+                    currentString.append(c);
+                } else {
+                    // terminate current string
+                    result.add(currentString.toString());
+                    currentString = new StringBuffer();
+                }
+            }
+                break;
+            default: {
+                // just copy over
+                currentString.append(c);
+            }
+                break;
+            }
+            index++;
+        }
+        result.add(currentString.toString());
+        return result.toArray(new String[result.size()]);
+    }
+    
+    /**
+     * @param line can be null
+     * @return the given line as excel-decoded list of Strings
+     */
+    public static List<String> excelDecodeRow(String line) {
+        if(line == null) {
+            return Collections.emptyList();
+        }
+        assert line != null;
+        List<String> row = new LinkedList<String>();
+        String[] datas = CsvCodec.splitAtUnquotedSemicolon(line);
+        for(int i = 0; i < datas.length; i++) {
+            String value = CsvCodec.excelDecode(datas[i]);
+            row.add(value);
+        }
+        return row;
+    }
+    
+    public static String excelEncodeRow(List<String> values) {
+        if(values == null) {
+            return "";
+        }
+        assert values != null;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < values.size(); i++) {
+            sb.append(CsvCodec.excelEncode(values.get(i)));
+            if(i + 1 < values.size()) {
+                sb.append(CELL_DELIMITER);
+            }
+        }
+        return sb.toString();
+    }
+    
+    // public static void main(String[] args) {
+    // System.out.println(excelDecodeRow(excelEncodeRow(Arrays.asList("\"a\"",
+    // null, "c"))));
+    // }
 }
