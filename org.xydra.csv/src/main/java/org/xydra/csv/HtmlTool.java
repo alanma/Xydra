8083f2f86fb62f5a1542aa00fe6b91bbb12adb17
Max Völkel <max.voelkel@calpano.com>
2013-02-13 16:22:48 +0000

    Added ability for custom cell render when writing as HTML

--- org.xydra.csv/src/main/java/org/xydra/csv/HtmlTool.java
+++ org.xydra.csv/src/main/java/org/xydra/csv/HtmlTool.java
@@ -12,53 +12,82 @@ import org.xydra.csv.impl.memory.Row;
 
 @RunsInGWT(false)
 public class HtmlTool {
-	
-	/**
-	 * Write the given table as simple HTML to the given writer.
-	 * 
-	 * @param table a table, never null
-	 * @param sortColName if this is not null, resulting HTML table will be
-	 *            sorted by the given column name
-	 * @param w never null
-	 * @throws IOException ...
-	 */
-	public static void writeToHtml(ISparseTable table, String sortColName, final Writer w)
-	        throws IOException {
-		w.write("<table class='csv'>\n");
-		w.write("<tr>");
-		
-		List<String> colNames = new ArrayList<String>();
-		colNames.addAll(table.getColumnNames());
-		// sort alphabetically
-		Collections.sort(colNames);
-		
-		for(String colName : colNames) {
-			w.write("<th>" + htmlencode(colName) + "</th>");
-		}
-		w.write("</tr>\n");
-		
-		Iterable<Row> rows = table;
-		if(sortColName != null) {
-			rows = TableTools.sortByColumn(table, sortColName);
-		}
-		
-		for(IRow row : rows) {
-			w.write("<tr>");
-			for(String colName : colNames) {
-				w.write("<td>" + htmlencode(row.getValue(colName)) + "</td>");
-			}
-			w.write("</tr>\n");
-		}
-		w.write("</table>\n");
-	}
-	
-	/**
-	 * @param s any string, might contain HTML-like stuff
-	 * @return string with basic XML encoding to prevent broken HTML
-	 */
-	public static String htmlencode(String s) {
-		return s.replace("&", "&apos;").replace("<", "&lt;").replace(">", "&gt;")
-		        .replace("\"", "&quot;").replace("'", "&apos;");
-	}
-	
+    
+    public static interface ICellRenderer {
+        String html(String cellContent);
+    }
+    
+    public static final ICellRenderer identityCellRenderer = new ICellRenderer() {
+        
+        @Override
+        public String html(String cellContent) {
+            return htmlencode(cellContent);
+        }
+    };
+    
+    /**
+     * Write the given table as simple HTML to the given writer.
+     * 
+     * @param table a table, never null
+     * @param sortColName if this is not null, resulting HTML table will be
+     *            sorted by the given column name
+     * @param w never null
+     * @throws IOException ...
+     */
+    public static void writeToHtml(ISparseTable table, String sortColName, final Writer w)
+            throws IOException {
+        writeToHtml(table, sortColName, w, identityCellRenderer);
+    }
+    
+    /**
+     * Write the given table as simple HTML to the given writer.
+     * 
+     * @param table a table, never null
+     * @param sortColName if this is not null, resulting HTML table will be
+     *            sorted by the given column name
+     * @param w never null
+     * @param cellRenderer
+     * @throws IOException ...
+     */
+    public static void writeToHtml(ISparseTable table, String sortColName, final Writer w,
+            ICellRenderer cellRenderer) throws IOException {
+        w.write("<table class='csv'>\n");
+        w.write("<tr>");
+        
+        List<String> colNames = new ArrayList<String>();
+        colNames.addAll(table.getColumnNames());
+        // sort alphabetically
+        Collections.sort(colNames);
+        
+        for(String colName : colNames) {
+            w.write("<th>" + htmlencode(colName) + "</th>");
+        }
+        w.write("</tr>\n");
+        
+        Iterable<Row> rows = table;
+        if(sortColName != null) {
+            rows = TableTools.sortByColumn(table, sortColName);
+        }
+        
+        for(IRow row : rows) {
+            w.write("<tr>");
+            for(String colName : colNames) {
+                String value = row.getValue(colName);
+                value = cellRenderer.html(value);
+                w.write("<td>" + value + "</td>");
+            }
+            w.write("</tr>\n");
+        }
+        w.write("</table>\n");
+    }
+    
+    /**
+     * @param s any string, might contain HTML-like stuff
+     * @return string with basic XML encoding to prevent broken HTML
+     */
+    public static String htmlencode(String s) {
+        return s.replace("&", "&apos;").replace("<", "&lt;").replace(">", "&gt;")
+                .replace("\"", "&quot;").replace("'", "&apos;");
+    }
+    
 }
