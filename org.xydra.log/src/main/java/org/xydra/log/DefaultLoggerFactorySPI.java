105db6bc485a625b2d32e631fda4263b575957ec
Max Völkel <max.voelkel@calpano.com>
2013-02-21 20:12:45 +0000

    Added support for logListeners in DefaultLogger

--- org.xydra.log/src/main/java/org/xydra/log/DefaultLoggerFactorySPI.java
+++ org.xydra.log/src/main/java/org/xydra/log/DefaultLoggerFactorySPI.java
@@ -7,31 +7,32 @@ import org.xydra.annotations.ThreadSafe;
 
 @ThreadSafe
 public class DefaultLoggerFactorySPI implements ILoggerFactorySPI {
-	
-	/*
-	 * TODO is there a reason why the methods don't care about the
-	 * listeners-parameter?
-	 */
-	
-	@Override
-	public Logger getLogger(String name, Collection<ILogListener> logListener) {
-		return new DefaultLogger(name);
-	}
-	
-	@Override
-	public Logger getThreadSafeLogger(String name, Collection<ILogListener> logListener) {
-		return new ThreadSafeDefaultLogger(name);
-	}
-	
-	@Override
-	public Logger getWrappedLogger(String name, String fullyQualifiedNameOfDelegatingLoggerClass) {
-		throw new UnsupportedOperationException();
-	}
-	
-	@Override
-	public Logger getThreadSafeWrappedLogger(String name,
-	        String fullyQualifiedNameOfDelegatingLoggerClass) {
-		throw new UnsupportedOperationException();
-	}
-	
+    
+    /*
+     * TODO is there a reason why the methods don't care about the
+     * listeners-parameter?
+     */
+    
+    @Override
+    public Logger getLogger(String name, Collection<ILogListener> logListener) {
+        return new DefaultLogger(name, logListener);
+    }
+    
+    @Override
+    public Logger getThreadSafeLogger(String name, Collection<ILogListener> logListener) {
+        // TODO implement logListeners
+        return new ThreadSafeDefaultLogger(name);
+    }
+    
+    @Override
+    public Logger getWrappedLogger(String name, String fullyQualifiedNameOfDelegatingLoggerClass) {
+        throw new UnsupportedOperationException();
+    }
+    
+    @Override
+    public Logger getThreadSafeWrappedLogger(String name,
+            String fullyQualifiedNameOfDelegatingLoggerClass) {
+        throw new UnsupportedOperationException();
+    }
+    
 }
