aa890300a12d5da853e2a72e1c0adab44ba2e561
Max Völkel <max.voelkel@calpano.com>
2013-02-28 21:46:59 +0000

    docu

--- org.xydra.core/src/main/java/org/xydra/base/value/XSingleValue.java
+++ org.xydra.core/src/main/java/org/xydra/base/value/XSingleValue.java
@@ -8,14 +8,14 @@ package org.xydra.base.value;
  * 
  */
 public interface XSingleValue<T> extends XValue {
-	
-	/**
-	 * @return This value in it's primitive type as determined by
-	 *         {@link ValueType#getPrimitiveType(ValueType)}. Implementations
-	 *         must guarantee that the original {@link XValue} can be
-	 *         reconstructed from the result of the primitive's toString()
-	 *         method if the type is known (and the value's content isn't null).
-	 */
-	T getValue();
-	
+    
+    /**
+     * @return This value in it's primitive type as determined by
+     *         {@link ValueType#getJavaClass()}. Implementations must guarantee
+     *         that the original {@link XValue} can be reconstructed from the
+     *         result of the primitive's toString() method if the type is known
+     *         (and the value's content isn't null).
+     */
+    T getValue();
+    
 }
