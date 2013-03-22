4dfb703dbea07566d855c55ffeb1db0856ae48b5
dressdotly@gmail.com <dressdotly@gmail.com@0ae76674-a2bc-2176-3712-945bbf24aeb5>
2013-03-22 16:47:33 +0000

    New test for synchronizer with multiple safe commands and fixCommands. Docu.

--- org.xydra.core/src/main/java/org/xydra/base/change/XAtomicCommand.java
+++ org.xydra.core/src/main/java/org/xydra/base/change/XAtomicCommand.java
@@ -5,20 +5,33 @@ package org.xydra.base.change;
  * XCommands}.
  */
 public interface XAtomicCommand extends XCommand {
-    
-    /**
-     * @return the current revision number of the entity this {@link XCommand}
-     *         refers to (for field commands) or adds/removes (for other
-     *         commands). Can also be {@link XCommand#FORCED} or
-     *         {@link XCommand#SAFE}.
-     */
-    long getRevisionNumber();
-    
-    /**
-     * @return true if this event should be applied even if the revision number
-     *         of the entity that will be changed by this command doesn't match
-     *         the revision number this command refers to.
-     */
-    boolean isForced();
-    
+	
+	/**
+	 * @return the current revision number of the entity this {@link XCommand}
+	 *         refers to (for field commands) or adds/removes (for other
+	 *         commands). Can also be {@link XCommand#FORCED} or
+	 *         {@link XCommand#SAFE}.
+	 * 
+	 *         More precisely: There are three kinds of commands: forced, safe
+	 *         bound to revisions, safe bound to state.
+	 * 
+	 *         Forced commands have the rev nr FORCED.
+	 * 
+	 *         safe with precondition have SAFE. Such commands are only executed
+	 *         if possible, e.g. an object add command is only executed as
+	 *         success, if the object was not there yet.
+	 * 
+	 *         All other rev nrs denote an expected rev nr. Only if the entitiy
+	 *         this command refers to has the expected given rev nr at execution
+	 *         time, then the command will be executed.
+	 */
+	long getRevisionNumber();
+	
+	/**
+	 * @return true if this event should be applied even if the revision number
+	 *         of the entity that will be changed by this command doesn't match
+	 *         the revision number this command refers to.
+	 */
+	boolean isForced();
+	
 }
