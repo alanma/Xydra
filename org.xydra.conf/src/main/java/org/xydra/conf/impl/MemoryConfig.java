742f27aa1edd30c3bb45ee886d10bdc7ad6766b6
Max Völkel <max.voelkel@calpano.com>
2013-03-28 06:46:46 +0000

    Better debugability

--- org.xydra.conf/src/main/java/org/xydra/conf/impl/MemoryConfig.java
+++ org.xydra.conf/src/main/java/org/xydra/conf/impl/MemoryConfig.java
@@ -13,32 +13,63 @@ import org.xydra.annotations.RunsInGWT;
 import org.xydra.conf.ConfBuilder;
 import org.xydra.conf.ConfigException;
 import org.xydra.conf.IConfig;
+import org.xydra.core.model.impl.memory.UUID;
 import org.xydra.index.impl.MapSetIndex;
 
 
 @RunsInGWT(true)
 public class MemoryConfig implements IConfig {
     
-    private HashMap<String,String> docs = new HashMap<String,String>();
+    /**
+     * Compile-time flag, should be false for high performance
+     */
+    private static boolean traceOrigins = true;
+    
+    public static String[] decodeList(String s) {
+        return s.split("[|]");
+    }
+    
+    public static String encodeList(String[] strings) {
+        StringBuilder b = new StringBuilder();
+        for(int i = 0; i < strings.length; i++) {
+            b.append(strings[i]);
+            if(i < strings.length)
+                b.append("|");
+        }
+        return b.toString();
+    }
     
     private TreeMap<String,String> defaults = new TreeMap<String,String>();
     
+    private HashMap<String,String> docs = new HashMap<String,String>();
+    
     private TreeMap<String,String> explicit = new TreeMap<String,String>();
+    
+    private final String internalId = UUID.uuid(4);
     
     private MapSetIndex<String,Class<?>> required = MapSetIndex.createWithFastEntrySets();
     
     private Set<Exception> setOrigins = new HashSet<Exception>();
     
-    private static boolean traceOrigins = true;
+    @Override
+    public void addRequiredSetting(String key, Class<?> caller) {
+        if(key == null)
+            throw new IllegalArgumentException("Key may not be null");
+        this.required.index(key, caller);
+    }
     
     @Override
-    public String get(String key) {
-        String value = tryToGet(key);
-        if(value == null) {
-            throw new ConfigException("Setting '" + key
-                    + "' requested but not set and no default set either.");
-        }
-        return value;
+    public void assertDefined(Enum<?> key) {
+        if(key == null)
+            throw new IllegalArgumentException("Key may not be null");
+        assertDefined(key.name());
+    }
+    
+    @Override
+    public void assertDefined(String key) {
+        if(key == null)
+            throw new IllegalArgumentException("Key may not be null");
+        get(key);
     }
     
     public String get(Enum<?> key) {
@@ -48,14 +79,26 @@ public class MemoryConfig implements IConfig {
     }
     
     @Override
-    public String tryToGet(String key) {
-        if(key == null)
-            throw new IllegalArgumentException("Key may not be null");
-        String value = this.explicit.get(key);
+    public String get(String key) {
+        String value = tryToGet(key);
         if(value == null) {
-            value = this.defaults.get(key);
+            throw new ConfigException("Setting '" + key
+                    + "' requested but not set and no default set either. " + idStr());
         }
         return value;
+    }
+    
+    @Override
+    public boolean getBoolean(Enum<?> key) {
+        if(key == null)
+            throw new IllegalArgumentException("Key may not be null");
+        return getBoolean(key.name());
+    }
+    
+    @Override
+    public boolean getBoolean(String key) {
+        String s = get(key);
+        return Boolean.parseBoolean(s);
     }
     
     @Override
@@ -67,8 +110,95 @@ public class MemoryConfig implements IConfig {
     }
     
     @Override
+    public String getDocumentation(String key) {
+        if(key == null)
+            throw new IllegalArgumentException("Key may not be null");
+        return this.docs.get(key);
+    }
+    
+    @Override
     public Iterable<String> getExplicitlyDefinedKeys() {
         return this.explicit.keySet();
+    }
+    
+    /**
+     * @return a short 4-character marker string to help identify which config
+     *         is which.
+     */
+    public String getInternalId() {
+        return this.internalId;
+    }
+    
+    @Override
+    public long getLong(Enum<?> key) {
+        if(key == null)
+            throw new IllegalArgumentException("Key may not be null");
+        return getLong(key.name());
+    }
+    
+    @Override
+    public long getLong(String key) {
+        String s = get(key);
+        return Long.parseLong(s);
+    }
+    
+    @Override
+    public Set<String> getMissingRequiredKeys() {
+        Set<String> open = new HashSet<String>(this.required.keySet());
+        open.removeAll(this.explicit.keySet());
+        open.removeAll(this.defaults.keySet());
+        return open;
+    }
+    
+    @Override
+    public Set<String> getRequiredKeys() {
+        return this.required.keySet();
+    }
+    
+    @Override
+    public String[] getStringArray(String key) {
+        String s = get(key);
+        if(s == null)
+            return null;
+        return decodeList(s);
+    }
+    
+    @Override
+    public Set<String> getStringSet(Enum<?> key) {
+        if(key == null)
+            throw new IllegalArgumentException("Key may not be null");
+        return getStringSet(key.name());
+    }
+    
+    @Override
+    public Set<String> getStringSet(String key) {
+        String[] array = getStringArray(key);
+        if(array == null)
+            array = new String[0];
+        return new HashSet<String>(Arrays.asList(array));
+    }
+    
+    private String idStr() {
+        return "[confId=" + getInternalId() + "]";
+    }
+    
+    @Override
+    public boolean isComplete() {
+        return getMissingRequiredKeys().isEmpty();
+    }
+    
+    @Override
+    public void revertToDefault(String key) {
+        if(key == null)
+            throw new IllegalArgumentException("Key may not be null");
+        this.explicit.remove(key);
+    }
+    
+    @Override
+    public ConfBuilder set(Enum<?> key, String value) {
+        if(key == null)
+            throw new IllegalArgumentException("Key may not be null");
+        return set(key.name(), value);
     }
     
     @Override
@@ -80,11 +210,25 @@ public class MemoryConfig implements IConfig {
     }
     
     @Override
+    public ConfBuilder setBoolean(Enum<?> key, boolean b) {
+        if(key == null)
+            throw new IllegalArgumentException("Key may not be null");
+        return setBoolean(key.name(), b);
+    }
+    
+    @Override
+    public ConfBuilder setBoolean(String key, boolean b) {
+        set(key, "" + b);
+        return new ConfBuilder(this, key);
+    }
+    
+    @Override
     public ConfBuilder setDefault(String key, String value, boolean initial) {
         if(key == null)
             throw new IllegalArgumentException("Key may not be null");
         if(!initial && this.defaults.containsKey(key))
-            throw new ConfigException("Setting '" + initial + "' had already a default value");
+            throw new ConfigException("Setting '" + initial + "' had already a default value "
+                    + idStr());
         this.defaults.put(key, value);
         if(traceOrigins) {
             try {
@@ -98,35 +242,10 @@ public class MemoryConfig implements IConfig {
     }
     
     @Override
-    public void revertToDefault(String key) {
+    public IConfig setDocumentation(Enum<?> key, String documentation) {
         if(key == null)
             throw new IllegalArgumentException("Key may not be null");
-        this.explicit.remove(key);
-    }
-    
-    @Override
-    public void addRequiredSetting(String key, Class<?> caller) {
-        if(key == null)
-            throw new IllegalArgumentException("Key may not be null");
-        this.required.index(key, caller);
-    }
-    
-    @Override
-    public Set<String> getRequiredKeys() {
-        return this.required.keySet();
-    }
-    
-    @Override
-    public Set<String> getMissingRequiredKeys() {
-        Set<String> open = new HashSet<String>(this.required.keySet());
-        open.removeAll(this.explicit.keySet());
-        open.removeAll(this.defaults.keySet());
-        return open;
-    }
-    
-    @Override
-    public boolean isComplete() {
-        return getMissingRequiredKeys().isEmpty();
+        return setDocumentation(key.name(), documentation);
     }
     
     @Override
@@ -138,34 +257,23 @@ public class MemoryConfig implements IConfig {
     }
     
     @Override
+    public ConfBuilder setLong(Enum<?> key, long value) {
+        if(key == null)
+            throw new IllegalArgumentException("Key may not be null");
+        return setLong(key.name(), value);
+    }
+    
+    @Override
     public ConfBuilder setLong(String key, long l) {
         set(key, "" + l);
         return new ConfBuilder(this, key);
     }
     
     @Override
-    public ConfBuilder setBoolean(String key, boolean b) {
-        set(key, "" + b);
-        return new ConfBuilder(this, key);
-    }
-    
-    @Override
-    public long getLong(String key) {
-        String s = get(key);
-        return Long.parseLong(s);
-    }
-    
-    @Override
-    public boolean getBoolean(String key) {
-        String s = get(key);
-        return Boolean.parseBoolean(s);
-    }
-    
-    @Override
-    public String getDocumentation(String key) {
+    public ConfBuilder setStrings(Enum<?> key, String ... values) {
         if(key == null)
             throw new IllegalArgumentException("Key may not be null");
-        return this.docs.get(key);
+        return setStrings(key.name(), values);
     }
     
     @Override
@@ -174,36 +282,6 @@ public class MemoryConfig implements IConfig {
         String s = encodeList(values);
         set(key, s);
         return new ConfBuilder(this, key);
-    }
-    
-    @Override
-    public String[] getStringArray(String key) {
-        String s = get(key);
-        if(s == null)
-            return null;
-        return decodeList(s);
-    }
-    
-    public static String encodeList(String[] strings) {
-        StringBuilder b = new StringBuilder();
-        for(int i = 0; i < strings.length; i++) {
-            b.append(strings[i]);
-            if(i < strings.length)
-                b.append("|");
-        }
-        return b.toString();
-    }
-    
-    public static String[] decodeList(String s) {
-        return s.split("[|]");
-    }
-    
-    @Override
-    public Set<String> getStringSet(String key) {
-        String[] array = getStringArray(key);
-        if(array == null)
-            array = new String[0];
-        return new HashSet<String>(Arrays.asList(array));
     }
     
     public String toString() {
@@ -221,73 +299,14 @@ public class MemoryConfig implements IConfig {
     }
     
     @Override
-    public ConfBuilder set(Enum<?> key, String value) {
+    public String tryToGet(String key) {
         if(key == null)
             throw new IllegalArgumentException("Key may not be null");
-        return set(key.name(), value);
-    }
-    
-    @Override
-    public long getLong(Enum<?> key) {
-        if(key == null)
-            throw new IllegalArgumentException("Key may not be null");
-        return getLong(key.name());
-    }
-    
-    @Override
-    public boolean getBoolean(Enum<?> key) {
-        if(key == null)
-            throw new IllegalArgumentException("Key may not be null");
-        return getBoolean(key.name());
-    }
-    
-    @Override
-    public IConfig setDocumentation(Enum<?> key, String documentation) {
-        if(key == null)
-            throw new IllegalArgumentException("Key may not be null");
-        return setDocumentation(key.name(), documentation);
-    }
-    
-    @Override
-    public ConfBuilder setStrings(Enum<?> key, String ... values) {
-        if(key == null)
-            throw new IllegalArgumentException("Key may not be null");
-        return setStrings(key.name(), values);
-    }
-    
-    @Override
-    public ConfBuilder setBoolean(Enum<?> key, boolean b) {
-        if(key == null)
-            throw new IllegalArgumentException("Key may not be null");
-        return setBoolean(key.name(), b);
-    }
-    
-    @Override
-    public ConfBuilder setLong(Enum<?> key, long value) {
-        if(key == null)
-            throw new IllegalArgumentException("Key may not be null");
-        return setLong(key.name(), value);
-    }
-    
-    @Override
-    public Set<String> getStringSet(Enum<?> key) {
-        if(key == null)
-            throw new IllegalArgumentException("Key may not be null");
-        return getStringSet(key.name());
-    }
-    
-    @Override
-    public void assertDefined(String key) {
-        if(key == null)
-            throw new IllegalArgumentException("Key may not be null");
-        get(key);
-    }
-    
-    @Override
-    public void assertDefined(Enum<?> key) {
-        if(key == null)
-            throw new IllegalArgumentException("Key may not be null");
-        assertDefined(key.name());
+        String value = this.explicit.get(key);
+        if(value == null) {
+            value = this.defaults.get(key);
+        }
+        return value;
     }
     
 }
