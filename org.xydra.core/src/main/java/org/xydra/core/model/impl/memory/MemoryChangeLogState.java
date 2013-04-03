f9c301fba245d7e182cbf910a4ffddd7afada0d9
dressdotly@gmail.com <dressdotly@gmail.com@0ae76674-a2bc-2176-3712-945bbf24aeb5>
2013-04-03 08:57:53 +0000

    Docu, readded assertion, and removed obsolete send sync events method

--- org.xydra.core/src/main/java/org/xydra/core/model/impl/memory/MemoryChangeLogState.java
+++ org.xydra.core/src/main/java/org/xydra/core/model/impl/memory/MemoryChangeLogState.java
@@ -11,100 +11,99 @@ import org.xydra.sharedutils.XyAssert;
 
 // TODO merge this into memory change log?
 public class MemoryChangeLogState implements XChangeLogState {
-    
-    private static final long serialVersionUID = 4745987477215964499L;
-    
-    /** the ID of the model this change log refers to **/
-    private XAddress baseAddr;
-    
-    /** the event list **/
-    private List<XEvent> events = new ArrayList<XEvent>();
-    
-    /**
-     * the revision number the model had when this changelog was created.
-     **/
-    private long baseRevisionNumber;
-    
-    /**
-     * Creates a new MemoryChangeLogState.
-     * 
-     * @param baseAddr The {@link XAddress} of the entity holding the
-     *            MemoryChangeLog which is represented by this
-     *            MemoryChangeLogState
-     */
-    public MemoryChangeLogState(XAddress baseAddr) {
-        this.baseAddr = baseAddr;
-    }
-    
-    @Override
-    public void appendEvent(XEvent event) {
-        
-        if(event == null) {
-            this.events.add(null);
-        } else {
-            
-            XyAssert.xyAssert(this.baseAddr.equalsOrContains(event.getChangedEntity()));
-            // FIXME MONKEY PATCHED XyAssert.xyAssert(event.getRevisionNumber()
-            // == getCurrentRevisionNumber() + 1);
-            XyAssert.xyAssert(!event.inTransaction());
-            
-            this.events.add(event);
-        }
-    }
-    
-    @Override
-    public XAddress getBaseAddress() {
-        return this.baseAddr;
-    }
-    
-    @Override
-    public long getCurrentRevisionNumber() {
-        return this.baseRevisionNumber + this.events.size();
-    }
-    
-    @Override
-    public XEvent getEvent(long revisionNumber) {
-        XEvent event = this.events.get((int)(revisionNumber - this.baseRevisionNumber - 1));
-        XyAssert.xyAssert(event == null || event.getRevisionNumber() == revisionNumber);
-        return event;
-    }
-    
-    @Override
-    public long getBaseRevisionNumber() {
-        return this.baseRevisionNumber;
-    }
-    
-    @Override
-    public void setBaseRevisionNumber(long rev) {
-        if(!this.events.isEmpty()) {
-            throw new IllegalStateException(
-                    "cannot set start revision number of non-empty change log");
-        }
-        this.baseRevisionNumber = rev;
-    }
-    
-    @Override
-    public String toString() {
-        return "change log for " + getBaseAddress() + ": baseRev=" + this.baseRevisionNumber
-                + " currentRev=" + getCurrentRevisionNumber() + " events=" + this.events.toString();
-    }
-    
-    @Override
-    public boolean truncateToRevision(long revisionNumber) {
-        if(revisionNumber > getCurrentRevisionNumber()) {
-            return false;
-        }
-        
-        if(revisionNumber < this.baseRevisionNumber) {
-            return false;
-        }
-        
-        while(revisionNumber < getCurrentRevisionNumber()) {
-            this.events.remove(this.events.size() - 1); // remove last element
-        }
-        
-        XyAssert.xyAssert(revisionNumber == getCurrentRevisionNumber());
-        
-        return true;
-    }
+	
+	private static final long serialVersionUID = 4745987477215964499L;
+	
+	/** the ID of the model this change log refers to **/
+	private XAddress baseAddr;
+	
+	/** the event list **/
+	private List<XEvent> events = new ArrayList<XEvent>();
+	
+	/**
+	 * the revision number the model had when this changelog was created.
+	 **/
+	private long baseRevisionNumber;
+	
+	/**
+	 * Creates a new MemoryChangeLogState.
+	 * 
+	 * @param baseAddr The {@link XAddress} of the entity holding the
+	 *            MemoryChangeLog which is represented by this
+	 *            MemoryChangeLogState
+	 */
+	public MemoryChangeLogState(XAddress baseAddr) {
+		this.baseAddr = baseAddr;
+	}
+	
+	@Override
+	public void appendEvent(XEvent event) {
+		
+		if(event == null) {
+			this.events.add(null);
+		} else {
+			
+			XyAssert.xyAssert(this.baseAddr.equalsOrContains(event.getChangedEntity()));
+			XyAssert.xyAssert(event.getRevisionNumber() == getCurrentRevisionNumber() + 1);
+			XyAssert.xyAssert(!event.inTransaction());
+			
+			this.events.add(event);
+		}
+	}
+	
+	@Override
+	public XAddress getBaseAddress() {
+		return this.baseAddr;
+	}
+	
+	@Override
+	public long getCurrentRevisionNumber() {
+		return this.baseRevisionNumber + this.events.size();
+	}
+	
+	@Override
+	public XEvent getEvent(long revisionNumber) {
+		XEvent event = this.events.get((int)(revisionNumber - this.baseRevisionNumber - 1));
+		XyAssert.xyAssert(event == null || event.getRevisionNumber() == revisionNumber);
+		return event;
+	}
+	
+	@Override
+	public long getBaseRevisionNumber() {
+		return this.baseRevisionNumber;
+	}
+	
+	@Override
+	public void setBaseRevisionNumber(long rev) {
+		if(!this.events.isEmpty()) {
+			throw new IllegalStateException(
+			        "cannot set start revision number of non-empty change log");
+		}
+		this.baseRevisionNumber = rev;
+	}
+	
+	@Override
+	public String toString() {
+		return "change log for " + getBaseAddress() + ": baseRev=" + this.baseRevisionNumber
+		        + " currentRev=" + getCurrentRevisionNumber() + " events=" + this.events.toString();
+	}
+	
+	@Override
+	public boolean truncateToRevision(long revisionNumber) {
+		if(revisionNumber > getCurrentRevisionNumber()) {
+			return false;
+		}
+		
+		if(revisionNumber < this.baseRevisionNumber) {
+			return false;
+		}
+		
+		while(revisionNumber < getCurrentRevisionNumber()) {
+			this.events.remove(this.events.size() - 1); // remove last element
+		}
+		
+		XyAssert.xyAssert(revisionNumber == getCurrentRevisionNumber());
+		
+		return true;
+	}
 }
