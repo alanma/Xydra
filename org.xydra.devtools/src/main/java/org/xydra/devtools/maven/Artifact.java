f318e0d593f63b29c71c113d240cf938ba083e63
dressdotly@gmail.com <dressdotly@gmail.com@0ae76674-a2bc-2176-3712-945bbf24aeb5>
2013-04-04 10:16:27 +0000

    Building Artifact graph

--- org.xydra.devtools/src/main/java/org/xydra/devtools/maven/Artifact.java
+++ org.xydra.devtools/src/main/java/org/xydra/devtools/maven/Artifact.java
@@ -1,15 +1,84 @@
 package org.xydra.devtools.maven;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
 
 
 public class Artifact {
-    
-    String artifactId;
-    String groupId;
-    String version;
-    
-    List<Artifact> dependencies = new ArrayList<Artifact>();
-    
+	String artifactId;
+	String groupId;
+	String version;
+	
+	Set<Artifact> dependencies = new HashSet<Artifact>();
+	
+	public Artifact(String artifactId, String groupId, String version) {
+		this.artifactId = artifactId;
+		this.groupId = groupId;
+		this.version = version;
+	}
+	
+	public void addDependency(Artifact artifact) {
+		this.dependencies.add(artifact);
+	}
+	
+	public String getArtifactId() {
+		return this.artifactId;
+	}
+	
+	public String getGroupId() {
+		return this.groupId;
+	}
+	
+	public String getVersion() {
+		return this.version;
+	}
+	
+	public Set<Artifact> getDependencies() {
+		return this.dependencies;
+	}
+	
+	public String id() {
+		return this.groupId + ":" + this.artifactId;
+	}
+	
+	@Override
+	public String toString() {
+		return id();
+	}
+	
+	@Override
+	public int hashCode() {
+		return id().hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof Artifact))
+			return false;
+		Artifact other = (Artifact)obj;
+		return id().equals(other.id());
+	}
+	
+	public static Artifact createWithoutDependencies(Model mavenModel) {
+		String artifactId = mavenModel.getArtifactId();
+		String groupId = mavenModel.getGroupId();
+		if(groupId == null) {
+			groupId = mavenModel.getParent().getGroupId();
+		}
+		if(groupId == null) {
+			System.out.println("Have no groupId for " + mavenModel);
+		}
+		String version = mavenModel.getVersion();
+		return new Artifact(artifactId, groupId, version);
+	}
+	
+	public static Artifact createWithoutDependencies(Dependency dependency) {
+		String artifactId = dependency.getArtifactId();
+		String groupId = dependency.getGroupId();
+		String version = dependency.getVersion();
+		return new Artifact(artifactId, groupId, version);
+	}
 }
--- org.xydra.devtools/src/main/java/org/xydra/devtools/maven/PomFinder.java
+++ org.xydra.devtools/src/main/java/org/xydra/devtools/maven/PomFinder.java
@@ -2,54 +2,121 @@ package org.xydra.devtools.maven;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.xydra.devtools.dot.Graph;
 
 
 public class PomFinder {
-    
-    public static List<File> findPomFiles(File dir) {
-        List<File> list = new LinkedList<File>();
-        File[] files = dir.listFiles(new FileFilter() {
-            
-            @Override
-            public boolean accept(File f) {
-                return f.isDirectory() || f.getName().equals("pom.xml");
-            }
-        });
-        for(File f : files) {
-            if(f.isDirectory()) {
-                list.addAll(findPomFiles(f));
-            } else {
-                list.add(f);
-            }
-        }
-        return list;
-    }
-    
-    public static void main(String[] args) {
-        showArtifactDependencies(new File("target/poms.dot"), new File("/Users/xamde/_data_/_p_"),
-                new File("/Users/xamde/.m2/repository"));
-    }
-    
-    private static void showArtifactDependencies(File targetDotFile, File ... sourceDirs) {
-        List<File> list = new LinkedList<File>();
-        for(File sourceDir : sourceDirs) {
-            list.addAll(findPomFiles(sourceDir));
-        }
-        
-        for(File f : list) {
-            parse(f);
-        }
-    }
-    
-    private static void parse(File f) {
-        System.out.println(f.getAbsolutePath());
-        
-        MavenXpp3Reader mavenReader = new MavenXpp3Reader();
-        Model mavenModel = mavenReader.read(r);
-    }
+	static Map<String,Artifact> artifactMap = new HashMap<String,Artifact>();
+	
+	public static List<File> findPomFiles(File dir) {
+		List<File> list = new LinkedList<File>();
+		File[] files = dir.listFiles(new FileFilter() {
+			
+			@Override
+			public boolean accept(File f) {
+				return f.isDirectory() || f.getName().equals("pom.xml");
+			}
+		});
+		for(File f : files) {
+			if(f.isDirectory()) {
+				list.addAll(findPomFiles(f));
+			} else {
+				list.add(f);
+			}
+		}
+		return list;
+	}
+	
+	public static void main(String[] args) {
+		showArtifactDependencies(new File("target/poms.dot"), new File("/home/alpha/workspace37"),
+		        new File("/home/alpha/.m2/repository"));
+	}
+	
+	private static void showArtifactDependencies(File targetDotFile, File ... sourceDirs) {
+		List<File> list = new LinkedList<File>();
+		for(File sourceDir : sourceDirs) {
+			list.addAll(findPomFiles(sourceDir));
+		}
+		
+		for(File f : list) {
+			try {
+				parse(f);
+			} catch(Exception e) {
+				System.out.println(e.toString());
+			}
+		}
+		// build graph
+		List<String> scope = new ArrayList<String>();
+		scope.add("com.calpano");
+		scope.add("org.xydra");
+		List<String> ignoreArtifacts = new ArrayList<String>();
+		ignoreArtifacts.add("com.calpano:scribble");
+		
+		Graph g = new Graph("poms");
+		for(Artifact artifact : artifactMap.values()) {
+			if(isArtifactWithinScope(artifact, scope)
+			        && !isArtifactIgnored(artifact, ignoreArtifacts)) {
+				for(Artifact depArtifact : artifact.dependencies) {
+					if(isArtifactWithinScope(depArtifact, scope))
+						g.addEdge("<" + artifact.toString().replace(":", "\n") + ">", "<"
+						        + depArtifact.toString().replace(":", "\n") + ">", 1, "\"\"");
+				}
+			}
+		}
+		
+		try {
+			g.writeTo(targetDotFile);
+		} catch(IOException e) {
+			System.out.println(e.toString());
+		}
+	}
+	
+	private static boolean isArtifactIgnored(Artifact artifact, List<String> ignoreArtifacts) {
+		return ignoreArtifacts.contains(artifact.id());
+	}
+	
+	private static boolean isArtifactWithinScope(Artifact artifact, List<String> scope) {
+		return scope.contains(artifact.getGroupId());
+	}
+	
+	private static void parse(File f) throws FileNotFoundException, IOException,
+	        XmlPullParserException {
+		System.out.println(f.getAbsolutePath());
+		
+		MavenXpp3Reader mavenReader = new MavenXpp3Reader();
+		
+		Model mavenModel = mavenReader.read(new FileInputStream(f));
+		Artifact artifact = Artifact.createWithoutDependencies(mavenModel);
+		artifact = getorPutArtifactReferenceViaMap(artifact);
+		
+		for(Dependency dependency : mavenModel.getDependencies()) {
+			Artifact depArtifact = Artifact.createWithoutDependencies(dependency);
+			depArtifact = getorPutArtifactReferenceViaMap(depArtifact);
+			artifact.addDependency(depArtifact);
+		}
+	}
+	
+	private static Artifact getorPutArtifactReferenceViaMap(Artifact lookForArtifact) {
+		Artifact artifact;
+		if(artifactMap.containsKey(lookForArtifact.id())) {
+			artifact = artifactMap.get(lookForArtifact.id());
+		} else {
+			artifactMap.put(lookForArtifact.id(), lookForArtifact);
+			artifact = lookForArtifact;
+		}
+		return artifact;
+	}
 }
