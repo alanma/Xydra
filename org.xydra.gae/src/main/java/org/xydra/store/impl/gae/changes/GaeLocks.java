aa549ac590ac0daded15d2e22a05367ce141ca09
Max Völkel <max.voelkel@calpano.com>
2013-05-25 16:07:42 +0000

    Allow RepositoryCommands in transactions

--- org.xydra.gae/src/main/java/org/xydra/store/impl/gae/changes/GaeLocks.java
+++ org.xydra.gae/src/main/java/org/xydra/store/impl/gae/changes/GaeLocks.java
@@ -17,7 +17,6 @@ import org.xydra.core.model.XField;
 import org.xydra.core.model.XModel;
 import org.xydra.core.model.XObject;
 import org.xydra.index.iterator.ReadOnlyIterator;
-import org.xydra.sharedutils.XyAssert;
 import org.xydra.store.impl.gae.GaeOperation;
 
 
@@ -36,186 +35,182 @@ import org.xydra.store.impl.gae.GaeOperation;
  * 
  */
 public class GaeLocks implements Iterable<XAddress> {
-	
-	public static final long serialVersionUID = 4334940263327007176L;
-	
-	private final Set<XAddress> locks;
-	
-	/**
-	 * Get the minimal locks needed to execute the given {@link XCommand}.
-	 * 
-	 * An address in the locks means that exclusive access is required to the
-	 * entity referred to by that address, as well as all descendant entities.
-	 * Also, a read lock on the ancestors of that entity is implied.
-	 * 
-	 * @param command The command to calculate the locks for.
-	 * @return never null
-	 */
-	@GaeOperation()
-	public static GaeLocks createLocks(XCommand command) {
-		GaeLocks gaeLocks = new GaeLocks();
-		if(command instanceof XTransaction) {
-			
-			XTransaction transaction = (XTransaction)command;
-			Set<XAddress> tempLocks = new HashSet<XAddress>();
-			for(XAtomicCommand atomicCommand : transaction) {
-				XyAssert.xyAssert(
-				        atomicCommand.getTarget().getAddressedType() != XType.XREPOSITORY,
-				        "A transaction cannot contain repository events");
-				
-				XAddress lock = atomicCommand.getChangedEntity();
-				assert lock != null;
-				/*
-				 * IMPROVE: ADD events don't need to lock the whole added entity
-				 * (they don't care if children change)
-				 */
-				tempLocks.add(lock);
-			}
-			for(XAddress lock : tempLocks) {
-				if(!hasMoreGeneralLock(tempLocks, lock)) {
-					gaeLocks.add(lock);
-				}
-			}
-			
-		} else {
-			XAddress lock = command.getChangedEntity();
-			assert lock != null;
-			gaeLocks.add(lock);
-		}
-		return gaeLocks;
-	}
-	
-	private void add(XAddress lockAddress) {
-		assert lockAddress != null;
-		this.locks.add(lockAddress);
-	}
-	
-	/**
-	 * Load a set of locks.
-	 * 
-	 * @param encodedLocks The locks to load.
-	 */
-	protected GaeLocks(List<String> encodedLocks) {
-		this.locks = new HashSet<XAddress>((int)(encodedLocks.size() / 0.75));
-		for(String s : encodedLocks) {
-			this.locks.add(XX.toAddress(s));
-		}
-	}
-	
-	public GaeLocks() {
-		this.locks = new HashSet<XAddress>();
-	}
-	
-	List<String> encode() {
-		List<String> lockStrs = new ArrayList<String>(this.locks.size());
-		for(XAddress a : this.locks) {
-			lockStrs.add(a.toURI());
-		}
-		return lockStrs;
-	}
-	
-	/**
-	 * @param addr
-	 * @return true if the specified locks are sufficient to write to the entity
-	 *         at the given address.
-	 */
-	public boolean canWrite(XAddress addr) {
-		for(XAddress lock : this.locks) {
-			if(lock.equalsOrContains(addr)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	/**
-	 * @param addr
-	 * @return true if the specified locks are sufficient to read from the
-	 *         entity at the given address.
-	 */
-	public boolean canRead(XAddress addr) {
-		for(XAddress lock : this.locks) {
-			if(addr.equalsOrContains(lock) || lock.contains(addr)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	/**
-	 * @param other
-	 * @return if the two sets of locks conflict, i.e. one of them requires a
-	 *         lock that is implied by the other set (wild-card locks are
-	 *         respected).
-	 */
-	public boolean isConflicting(GaeLocks other) {
-		for(XAddress lock : this.locks) {
-			if(other.locks.contains(lock) || hasMoreGeneralLock(other.locks, lock)) {
-				return true;
-			}
-		}
-		for(XAddress lock : other.locks) {
-			if(hasMoreGeneralLock(this.locks, lock)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	/**
-	 * Handles wild-cards in locks.
-	 * 
-	 * @return true if the given set contains any locks that imply the given
-	 *         lock (but are not the same).
-	 */
-	static private boolean hasMoreGeneralLock(Set<XAddress> locks, XAddress lock) {
-		XAddress l = lock.getParent();
-		while(l != null) {
-			if(locks.contains(l)) {
-				return true;
-			}
-			l = l.getParent();
-		}
-		return false;
-	}
-	
-	/**
-	 * @param addr
-	 * @return true if the specified locks are sufficient to delete the entity
-	 *         at the given address.
-	 */
-	public boolean canRemove(XAddress addr) {
-		return canWrite(addr);
-	}
-	
-	/**
-	 * @return number of locks
-	 */
-	public int size() {
-		return this.locks.size();
-	}
-	
-	@Override
-	public Iterator<XAddress> iterator() {
-		return new ReadOnlyIterator<XAddress>(this.locks.iterator());
-	}
-	
-	@Override
-	public String toString() {
-		return Arrays.toString(encode().toArray());
-	}
-	
-	/**
-	 * @return if a complete model lock is represented
-	 */
-	public boolean isLockingTheModel() {
-		if(this.locks.size() == 1) {
-			XAddress xa = this.locks.iterator().next();
-			if(xa.getAddressedType() == XType.XMODEL) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
+    
+    public static final long serialVersionUID = 4334940263327007176L;
+    
+    private final Set<XAddress> locks;
+    
+    /**
+     * Get the minimal locks needed to execute the given {@link XCommand}.
+     * 
+     * An address in the locks means that exclusive access is required to the
+     * entity referred to by that address, as well as all descendant entities.
+     * Also, a read lock on the ancestors of that entity is implied.
+     * 
+     * @param command The command to calculate the locks for.
+     * @return never null
+     */
+    @GaeOperation()
+    public static GaeLocks createLocks(XCommand command) {
+        GaeLocks gaeLocks = new GaeLocks();
+        if(command instanceof XTransaction) {
+            
+            XTransaction transaction = (XTransaction)command;
+            Set<XAddress> tempLocks = new HashSet<XAddress>();
+            for(XAtomicCommand atomicCommand : transaction) {
+                XAddress lock = atomicCommand.getChangedEntity();
+                assert lock != null;
+                /*
+                 * IMPROVE: ADD events don't need to lock the whole added entity
+                 * (they don't care if children change)
+                 */
+                tempLocks.add(lock);
+            }
+            for(XAddress lock : tempLocks) {
+                if(!hasMoreGeneralLock(tempLocks, lock)) {
+                    gaeLocks.add(lock);
+                }
+            }
+            
+        } else {
+            XAddress lock = command.getChangedEntity();
+            assert lock != null;
+            gaeLocks.add(lock);
+        }
+        return gaeLocks;
+    }
+    
+    private void add(XAddress lockAddress) {
+        assert lockAddress != null;
+        this.locks.add(lockAddress);
+    }
+    
+    /**
+     * Load a set of locks.
+     * 
+     * @param encodedLocks The locks to load.
+     */
+    protected GaeLocks(List<String> encodedLocks) {
+        this.locks = new HashSet<XAddress>((int)(encodedLocks.size() / 0.75));
+        for(String s : encodedLocks) {
+            this.locks.add(XX.toAddress(s));
+        }
+    }
+    
+    public GaeLocks() {
+        this.locks = new HashSet<XAddress>();
+    }
+    
+    List<String> encode() {
+        List<String> lockStrs = new ArrayList<String>(this.locks.size());
+        for(XAddress a : this.locks) {
+            lockStrs.add(a.toURI());
+        }
+        return lockStrs;
+    }
+    
+    /**
+     * @param addr
+     * @return true if the specified locks are sufficient to write to the entity
+     *         at the given address.
+     */
+    public boolean canWrite(XAddress addr) {
+        for(XAddress lock : this.locks) {
+            if(lock.equalsOrContains(addr)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * @param addr
+     * @return true if the specified locks are sufficient to read from the
+     *         entity at the given address.
+     */
+    public boolean canRead(XAddress addr) {
+        for(XAddress lock : this.locks) {
+            if(addr.equalsOrContains(lock) || lock.contains(addr)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * @param other
+     * @return if the two sets of locks conflict, i.e. one of them requires a
+     *         lock that is implied by the other set (wild-card locks are
+     *         respected).
+     */
+    public boolean isConflicting(GaeLocks other) {
+        for(XAddress lock : this.locks) {
+            if(other.locks.contains(lock) || hasMoreGeneralLock(other.locks, lock)) {
+                return true;
+            }
+        }
+        for(XAddress lock : other.locks) {
+            if(hasMoreGeneralLock(this.locks, lock)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * Handles wild-cards in locks.
+     * 
+     * @return true if the given set contains any locks that imply the given
+     *         lock (but are not the same).
+     */
+    static private boolean hasMoreGeneralLock(Set<XAddress> locks, XAddress lock) {
+        XAddress l = lock.getParent();
+        while(l != null) {
+            if(locks.contains(l)) {
+                return true;
+            }
+            l = l.getParent();
+        }
+        return false;
+    }
+    
+    /**
+     * @param addr
+     * @return true if the specified locks are sufficient to delete the entity
+     *         at the given address.
+     */
+    public boolean canRemove(XAddress addr) {
+        return canWrite(addr);
+    }
+    
+    /**
+     * @return number of locks
+     */
+    public int size() {
+        return this.locks.size();
+    }
+    
+    @Override
+    public Iterator<XAddress> iterator() {
+        return new ReadOnlyIterator<XAddress>(this.locks.iterator());
+    }
+    
+    @Override
+    public String toString() {
+        return Arrays.toString(encode().toArray());
+    }
+    
+    /**
+     * @return if a complete model lock is represented
+     */
+    public boolean isLockingTheModel() {
+        if(this.locks.size() == 1) {
+            XAddress xa = this.locks.iterator().next();
+            if(xa.getAddressedType() == XType.XMODEL) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
 }
