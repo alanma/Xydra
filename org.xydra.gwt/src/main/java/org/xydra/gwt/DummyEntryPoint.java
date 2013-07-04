9683a77d272c4b509a0027d8746570496a683bfb
Max Völkel <max.voelkel@calpano.com>
2013-07-04 10:16:10 +0000

    Xydra 0.1.7-SNAPSHOT

---
+++ org.xydra.gwt/src/main/java/org/xydra/gwt/DummyEntryPoint.java
@@ -0,0 +1,15 @@
+package org.xydra.gwt;
+
+import org.xydra.annotations.ThreadSafe;
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+@ThreadSafe
+public class DummyEntryPoint implements EntryPoint {
+	
+	@Override
+	public void onModuleLoad() {
+	}
+	
+}
---
+++ org.xydra.gwt/src/test/java/org/xydra/gwt/GwtDISABLEDTestStore.java
@@ -0,0 +1,28 @@
+package org.xydra.gwt;
+
+import org.junit.Ignore;
+
+
+/**
+ * By default, the gwt-maven-plugin uses GwtTest*.java as inclusion pattern so
+ * that such tests will not match the standard Surefire pattern.
+ */
+@Ignore
+public class GwtDISABLEDTestStore // FIXME extends GWTTestCase
+{
+    
+    // @Override
+    // public String getModuleName() {
+    // return "org.xydra.gwt.XydraGwtTest";
+    // }
+    //
+    // @Test
+    // @Ignore
+    // public void test() {
+    //
+    // XydraParser parser = new GwtXmlParser();
+    // assertNotNull(parser);
+    //
+    // }
+    
+}
