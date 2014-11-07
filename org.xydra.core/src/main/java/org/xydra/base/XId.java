85677589cdc1e8da5d5b84006d81b11bcef041b9
Max Völkel <max.voelkel@calpano.com>
2014-11-07 14:56:09 +0100

    core: Xid no longer extends SimpleValue<T>; it's special now. Allows implementations to implement Xid with even more flexibility.

--- org.xydra.core/src/main/java/org/xydra/base/XId.java
+++ org.xydra.core/src/main/java/org/xydra/base/XId.java
@@ -2,8 +2,7 @@ package org.xydra.base;
 
 import java.io.Serializable;
 
-import org.xydra.base.value.XSingleValue;
-
+import org.xydra.base.value.XValue;
 
 /**
  * A identification object that can be serialised.
@@ -15,24 +14,31 @@ import org.xydra.base.value.XSingleValue;
  * 
  * @author xamde
  */
-public interface XId extends XSingleValue<XId>, Serializable, Comparable<XId>, IHasXId {
-    
-    /**
-     * A default Id to be used in cases where a single default Id solves the
-     * problem. E.g. to be used as the repository-Id of a model that has no
-     * repository.
-     */
-    XId DEFAULT = Base.toId("_default");
-    
-    /**
-     * To convert this XId to a byte[] representation use the ByteUtils (TBD).
-     * 
-     * @return a compact String which can be turned into an XId again via. The
-     *         length of the string SHOULD be at most 100 characters for maximal
-     *         compatibility with all kinds of back-ends such as Google
-     *         AppEngine. {@link XIdProvider#fromString(String)}
-     */
-    @Override
-    String toString();
-    
+public interface XId extends XValue, Serializable, Comparable<XId>, IHasXId {
+
+	/**
+	 * A default Id to be used in cases where a single default Id solves the
+	 * problem. E.g. to be used as the repository-Id of a model that has no
+	 * repository.
+	 */
+	XId DEFAULT = Base.toId("_default");
+
+	/**
+	 * To convert this XId to a byte[] representation use the ByteUtils (TBD).
+	 * 
+	 * @return a compact String which can be turned into an XId again via. The
+	 *         length of the string SHOULD be at most 100 characters for maximal
+	 *         compatibility with all kinds of back-ends such as Google
+	 *         AppEngine. {@link XIdProvider#fromString(String)}
+	 */
+	@Override
+	String toString();
+
+	/**
+	 * The id as a binary representation
+	 * 
+	 * @return a byte array, in which no byte is 00 (NUL)
+	 */
+	byte[] toBytes();
+
 }
