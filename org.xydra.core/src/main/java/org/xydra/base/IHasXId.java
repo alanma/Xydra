1c275429aab7434b17efaa0dcfea639e9ee434ec
Max Völkel <max.voelkel@calpano.com>
2014-11-11 13:14:22 +0100

    core: doc

--- org.xydra.core/src/main/java/org/xydra/base/IHasXId.java
+++ org.xydra.core/src/main/java/org/xydra/base/IHasXId.java
@@ -1,7 +1,7 @@
 package org.xydra.base;
 
+import org.xydra.annotations.NeverNull;
 import org.xydra.annotations.ReadOperation;
-
 
 /**
  * Objects of classes that implement this interface have an {@link XId}.
@@ -9,14 +9,15 @@ import org.xydra.annotations.ReadOperation;
  * @author xamde
  */
 public interface IHasXId {
-	
+
 	/**
 	 * Returns the {@link XId} of this entity.
 	 * 
-	 * @return The {@link XId} of this entity.
+	 * @return The {@link XId} of this entity. @NeverNull
 	 * @throws IllegalStateException if this entity has already been removed
 	 */
 	@ReadOperation
+	@NeverNull
 	XId getId();
-	
+
 }
