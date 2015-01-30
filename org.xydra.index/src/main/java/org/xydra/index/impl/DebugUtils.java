d6345baf3ed769bc1c85e1c949e6cb41450215ec
Max Völkel <max.voelkel@calpano.com>
2015-01-30 22:03:20 +0100

    refactored toLimitedString(..)

--- org.xydra.index/src/main/java/org/xydra/index/impl/DebugUtils.java
+++ org.xydra.index/src/main/java/org/xydra/index/impl/DebugUtils.java
@@ -4,15 +4,19 @@ public class DebugUtils {
 
 	/**
 	 * @param object
-	 *            @CanBeNull
-	 * @param maxLen
-	 *            use -1 for unlimited
+	 * @CanBeNull
+	 * @param maxLen use -1 for unlimited
 	 * @return a String with max len 'maxLen'
 	 */
 	public static String toLimitedString(Object object, int maxLen) {
 		if (object == null)
 			return "-null-";
 		String s = object.toString();
+		return toLimited(s, maxLen);
+	}
+
+	public static String toLimited(String s, int maxLen) {
+		assert s != null;
 		if (maxLen >= 0 && s.length() > maxLen) {
 			if (maxLen >= "[...]".length())
 				return s.substring(0, maxLen - "[...]".length()) + "[...]";
