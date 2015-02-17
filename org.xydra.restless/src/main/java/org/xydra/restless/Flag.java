92517b912dabae3e61c0b1934852262f48b43a39
Max Völkel <max.voelkel@calpano.com>
2015-02-17 23:23:36 +0100

    Restless: introduced a call-by-reference boolean

---
+++ org.xydra.restless/src/main/java/org/xydra/restless/Flag.java
@@ -0,0 +1,32 @@
+package org.xydra.restless;
+
+/**
+ * A boolean flag, for call by reference
+ * 
+ * @author xamde
+ */
+public class Flag {
+
+	public Flag(boolean value) {
+		this.value = value;
+	}
+
+	private boolean value;
+
+	public void setTrue() {
+		this.value = true;
+	}
+
+	public void setFalse() {
+		this.value = false;
+	}
+
+	public boolean getValue() {
+		return this.value;
+	}
+
+	@Override
+	public String toString() {
+		return Boolean.toString(getValue());
+	}
+}
