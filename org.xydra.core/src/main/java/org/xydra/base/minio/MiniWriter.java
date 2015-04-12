261291bbfd103315f201065fa5014c2efb941be5
Max Völkel <max.voelkel@calpano.com>
2015-04-12 18:19:09 +0200

    doc

--- org.xydra.core/src/main/java/org/xydra/base/minio/MiniWriter.java
+++ org.xydra.core/src/main/java/org/xydra/base/minio/MiniWriter.java
@@ -4,18 +4,27 @@ import org.xydra.annotations.RequiresAppEngine;
 import org.xydra.annotations.RunsInAppEngine;
 import org.xydra.annotations.RunsInGWT;
 
-
 @RunsInGWT(true)
 @RunsInAppEngine(true)
 @RequiresAppEngine(false)
 public interface MiniWriter {
-	
+
 	void write(String string) throws MiniIOException;
-	
+
 	void write(char c) throws MiniIOException;
-	
+
+	/**
+	 * Flushes the underlying stream, if there is one
+	 * 
+	 * @throws MiniIOException
+	 */
 	void flush() throws MiniIOException;
-	
+
+	/**
+	 * Closes the underlying stream, if there is one
+	 * 
+	 * @throws MiniIOException
+	 */
 	void close() throws MiniIOException;
-	
+
 }
