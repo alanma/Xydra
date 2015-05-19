4b0da406bb73317a0654f66c7ce5b8b20edc3dd2
Max Völkel <max.voelkel@calpano.com>
2015-05-19 15:06:57 +0200

    entityExists is no longer transient

--- org.xydra.core/src/main/java/org/xydra/base/rmof/impl/memory/SimpleEntity.java
+++ org.xydra.core/src/main/java/org/xydra/base/rmof/impl/memory/SimpleEntity.java
@@ -6,37 +6,37 @@ import org.xydra.base.rmof.XEntity;
 
 
 public abstract class SimpleEntity implements Serializable, XEntity {
-    
-    private static final long serialVersionUID = -8935900909094851790L;
-    
-    /* Just for GWT */
-    protected SimpleEntity() {
-    }
-    
-    @Override
-    public int hashCode() {
-        return (int)(this.getAddress().hashCode() + this.getRevisionNumber());
-    }
-    
-    public abstract long getRevisionNumber();
-    
-    private transient boolean entityExists = true;
-    
-    /**
-     * @return at runtime if the entity currently exists or not. This flag is
-     *         not persisted.
-     */
-    public boolean exists() {
-        return this.entityExists;
-    }
-    
-    /**
-     * Set if this entity exists or not. This flag is not persisted.
-     * 
-     * @param b
-     */
-    public void setExists(boolean b) {
-        this.entityExists = b;
-    }
-    
+
+	private static final long serialVersionUID = -8935900909094851790L;
+
+	/* Just for GWT */
+	protected SimpleEntity() {
+	}
+
+	@Override
+	public int hashCode() {
+		return (int) (this.getAddress().hashCode() + this.getRevisionNumber());
+	}
+
+	public abstract long getRevisionNumber();
+
+	private boolean entityExists = true;
+
+	/**
+	 * @return at runtime if the entity currently exists or not. This flag is
+	 *         not persisted.
+	 */
+	public boolean exists() {
+		return this.entityExists;
+	}
+
+	/**
+	 * Set if this entity exists or not. This flag is not persisted.
+	 * 
+	 * @param b
+	 */
+	public void setExists(boolean b) {
+		this.entityExists = b;
+	}
+
 }
