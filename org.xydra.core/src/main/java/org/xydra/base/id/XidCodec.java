1909eece1281b69ff2aff372050a7b27897c7ec3
Max Völkel <max.voelkel@calpano.com>
2015-07-01 18:37:47 +0200

    XidCodec: bugfix. Overly long IDs can now always be decoded;

--- org.xydra.core/src/main/java/org/xydra/base/id/XidCodec.java
+++ org.xydra.core/src/main/java/org/xydra/base/id/XidCodec.java
@@ -8,9 +8,9 @@ import org.xydra.index.impl.IntegerRangeIndex;
 
 /**
  * Static methods.
- * 
+ *
  * Encodes strings as XIds (within possible limits)
- * 
+ *
  * @author xamde
  */
 @RunsInGWT(false)
@@ -32,36 +32,54 @@ public class XidCodec {
 		NEEDS_NO_ENCODING_firstChar = new IntegerRangeIndex();
 		NEEDS_NO_ENCODING_firstChar.addAll(BaseStringIDProvider.RANGEINDEX_nameStartChar);
 		assert BaseStringIDProvider.RANGEINDEX_nameStartChar.isInInterval(ENCODING_CHAR);
-		NEEDS_NO_ENCODING_firstChar.deIndex(ENCODING_CHAR, ENCODING_CHAR);
+		NEEDS_NO_ENCODING_firstChar.deIndex(ENCODING_CHAR);
 
 		NEEDS_NO_ENCODING_nthChar = new IntegerRangeIndex();
 		NEEDS_NO_ENCODING_nthChar.addAll(BaseStringIDProvider.RANGEINDEX_nameChar);
 		assert BaseStringIDProvider.RANGEINDEX_nameChar.isInInterval(ENCODING_CHAR);
-		NEEDS_NO_ENCODING_nthChar.deIndex(ENCODING_CHAR, ENCODING_CHAR);
+		NEEDS_NO_ENCODING_nthChar.deIndex(ENCODING_CHAR);
+
+		assert!NEEDS_NO_ENCODING_firstChar.isInInterval(ENCODING_CHAR);
+		assert!NEEDS_NO_ENCODING_nthChar.isInInterval(ENCODING_CHAR);
 	}
 
-	private static void appendEncoded(StringBuilder b, int codepoint) {
+	/**
+	 * @param b
+	 * @param codepoint
+	 * @param maxLen 1.. {@link XIdProvider#MAX_LENGTH}
+	 * @throws IllegalStateException if maxLen would not be respected
+	 */
+	public static void appendEncoded(final StringBuilder b, final int codepoint, final int maxLen) throws IllegalStateException {
 		/*
 		 * let's be clever here: encoding in range 0..255 is represented as
 		 * ENCODING_CHAR + 2 hex;
-		 * 
+		 *
 		 * 255.. 65535 is ENCODING_CHAR + ENCODING_CHAR + 4 hex
-		 * 
+		 *
 		 * range 65536..1114111 is represented as ENCODING_CHAR + ENCODING_CHAR
 		 * + ENCODING_CHAR + 6 hex
 		 */
 
 		if (codepoint <= 255) {
+			if (b.length() + 3 > maxLen)
+				throw new IllegalStateException(
+						"maxLen(" + maxLen + ") does not allow adding 3 more characters");
 			b.appendCodePoint(ENCODING_CHAR);
 			// hack to get padding with leading zeroes
 			b.append(Integer.toHexString(0x100 | codepoint).substring(1));
 
 		} else if (codepoint <= 65535) {
+			if (b.length() + 6 > maxLen)
+				throw new IllegalStateException(
+						"maxLen(" + maxLen + ") does not allow adding 6 more characters");
 			b.appendCodePoint(ENCODING_CHAR);
 			b.appendCodePoint(ENCODING_CHAR);
 			// hack to get padding with leading zeroes
 			b.append(Integer.toHexString(0x10000 | codepoint).substring(1));
 		} else {
+			if (b.length() + 9 > maxLen)
+				throw new IllegalStateException(
+						"maxLen(" + maxLen + ") does not allow adding 9 more characters");
 			b.appendCodePoint(ENCODING_CHAR);
 			b.appendCodePoint(ENCODING_CHAR);
 			b.appendCodePoint(ENCODING_CHAR);
@@ -76,14 +94,14 @@ public class XidCodec {
 	 * contain '_' for other reasons. So if this method is invoked on a
 	 * non-encoded string containing '_'s, the original string should be
 	 * returned.
-	 * 
+	 *
 	 * @param encId @NeverNull
 	 * @return a decoded idString
 	 */
-	public static String decode(String encId) {
+	public static String decode(final String encId) {
 		assert encId != null && encId.length() > 0;
 
-		StringBuilder b = new StringBuilder();
+		final StringBuilder b = new StringBuilder();
 
 		int i = 0;
 		while (i < encId.length()) {
@@ -94,42 +112,39 @@ public class XidCodec {
 				int markers = 0;
 				do {
 					i += Character.charCount(codePoint);
-					if (i >= encId.length()) {
+					if (i >= encId.length())
 						// was not really a xid-encoded id
 						return encId;
-					}
 					codePoint = encId.codePointAt(i);
 					markers++;
 				} while (codePoint == ENCODING_CHAR);
 				// we moved past the markers
 
-				if (markers > 3) {
+				if (markers > 3)
 					throw new IllegalArgumentException("String has too many '" + ENCODING_CHAR
 							+ "', we allow at most 3 for the hex-encoding. String is '" + encId
 							+ "'");
-				}
-				int expectHex = 2 * markers;
+				final int expectHex = 2 * markers;
 				// hex chars are code point count = char count
 				if (i - Character.charCount(codePoint) + expectHex < encId.length()) {
-					String hex = encId.substring(i, i + expectHex);
+					final String hex = encId.substring(i, i + expectHex);
 					i += expectHex;
 					try {
-						int decodedCodepoint = Integer.parseInt(hex, 16);
+						final int decodedCodepoint = Integer.parseInt(hex, 16);
 						b.appendCodePoint(decodedCodepoint);
-					} catch (NumberFormatException e) {
+					} catch (final NumberFormatException e) {
 						// not really encoded? copy verbatim.
 						// was not really a xid-encoded id
 						return encId;
-					} catch (IllegalArgumentException e) {
+					} catch (final IllegalArgumentException e) {
 						// not really encoded? copy verbatim.
 						// was not really a xid-encoded id
 						return encId;
 					}
-				} else {
+				} else
 					// cannot be a proper hex encoding, roll-back
 					// was not really a xid-encoded id
 					return encId;
-				}
 			} else {
 				// copy over
 				b.appendCodePoint(codePoint);
@@ -141,12 +156,12 @@ public class XidCodec {
 
 	/**
 	 * Helps debugging
-	 * 
+	 *
 	 * @param xid
 	 * @return the decoded id string
 	 */
-	public static String decodeFromXId(XId xid) {
-		String s = xid.toString();
+	public static String decodeFromXId(final XId xid) {
+		final String s = xid.toString();
 
 		if (s == null || s.length() == 0)
 			throw new IllegalArgumentException("Cannot decode null or empty");
@@ -154,53 +169,57 @@ public class XidCodec {
 		return decode(s);
 	}
 
-	public static String encode(String s, int maxLen) {
-		StringBuilder enc = new StringBuilder();
+	/**
+	 * @param s
+	 * @param maxLen must be 1 .. x
+	 * @return
+	 */
+	public static String encode(final String s, final int maxLen) {
+		assert maxLen > 0;
+		final StringBuilder enc = new StringBuilder();
 
 		// first char is pretty strict
-		int firstCodepoint = s.codePointAt(0);
+		final int firstCodepoint = s.codePointAt(0);
 		if (NEEDS_NO_ENCODING_firstChar.isInInterval(firstCodepoint)) {
 			// first char is legal, copy over
+			assert enc.length() + 1 <= maxLen;
 			enc.appendCodePoint(firstCodepoint);
 		} else {
 			// needs encoding
-			appendEncoded(enc, firstCodepoint);
+			appendEncoded(enc, firstCodepoint, maxLen);
 		}
 
 		// nth chars are a bit more liberal
 		int i = Character.charCount(firstCodepoint);
 		while (i < s.length() && enc.length() < maxLen) {
-			int nthCodepoint = s.codePointAt(i);
+			if(enc.length() == maxLen) {
+				break;
+			}
+
+			final int nthCodepoint = s.codePointAt(i);
 			if (NEEDS_NO_ENCODING_nthChar.isInInterval(nthCodepoint)) {
 				// copy over
 				enc.appendCodePoint(nthCodepoint);
 			} else {
 				// needs encoding
-				appendEncoded(enc, nthCodepoint);
+				try {
+					appendEncoded(enc, nthCodepoint, maxLen);
+				} catch (final IllegalStateException e) {
+					break;
+				}
 			}
 			i += Character.charCount(nthCodepoint);
 		}
 
-		// drastic: truncate to max length
-		if (enc.length() > maxLen) {
-			enc.setLength(maxLen);
-		}
-
-		if (enc.length() > XIdProvider.MAX_LENGTH) {
-			throw new IllegalArgumentException("Input string '" + s
-					+ "' results in overly long encoded string '" + enc
-					+ "', cannot create XId from it.");
-		}
-
-		String encString = enc.toString();
+		final String encString = enc.toString();
 		assert MemoryStringIDProvider.isValidId(encString) : "Failed to encode '" + s + "' as '"
-				+ encString + "'";
+		+ encString + "'";
 		return encString;
 	}
 
 	/**
 	 * Encodes only those characters that need it.
-	 * 
+	 *
 	 * @param s @NeverNull
 	 * @param maxLen use a number longer than {@link XIdProvider#MAX_LENGTH} to
 	 *            get an exception when string is too long
@@ -208,43 +227,16 @@ public class XidCodec {
 	 * @throws IllegalArgumentException if input string is null, empty or too
 	 *             long
 	 */
-	public static XId encodeAsXId(String s, int maxLen) throws IllegalArgumentException {
-		if (s == null) {
+	public static XId encodeAsXId(final String s, final int maxLen)
+			throws IllegalArgumentException {
+		if (s == null)
 			throw new IllegalArgumentException("Given string is null");
-		}
-		if (s.length() == 0) {
+		if (s.length() == 0)
 			throw new IllegalArgumentException("Given string is empty");
-		}
 
-		String encString = encode(s, maxLen);
+		final String encString = encode(s, maxLen);
 		// IMPROVE do without checks
 		return XX.toId(encString);
-	}
-
-	public static void main(String[] args) {
-
-		System.out.println("159 = '" + ((char) 159) + "' = a unicode control character");
-		System.out.println("159 = '" + ((char) 159) + "'");
-
-		StringBuilder b = new StringBuilder();
-		appendEncoded(b, 1);
-		b.append(" | ");
-		appendEncoded(b, 1042);
-		b.append(" | ");
-		appendEncoded(b, 65699);
-		System.out.println(b);
-
-		System.out.println(XidCodec.encodeAsXId("40", XIdProvider.MAX_LENGTH));
-
-		String[] s = new String[] {
-
-		"Self-organized+Reuse+of+Software+Engineering+Knowledge+Supported+by+Semantic+Wikis",
-				"CallAutomata?)>\\" };
-		for (String a : s) {
-			System.out.println(XidCodec.encodeAsXId(a, XIdProvider.MAX_LENGTH));
-		}
-		System.out.println(decode("_30install"));
-		System.out.println(decode("_meta"));
 	}
 
 	// // combine truncated (=easy debugging) + hash (=better
---
+++ org.xydra.core/src/test/java/org/xydra/base/id/XidCodecTest.java
@@ -0,0 +1,182 @@
+package org.xydra.base.id;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.xydra.base.XId;
+import org.xydra.base.XIdProvider;
+import org.xydra.log.api.Logger;
+import org.xydra.log.api.LoggerFactory;
+
+import com.google.gwt.regexp.shared.MatchResult;
+import com.google.gwt.regexp.shared.RegExp;
+
+public class XidCodecTest {
+
+	@SuppressWarnings("unused")
+	private static final Logger log = LoggerFactory.getLogger(XidCodecTest.class);
+
+	static class Sample {
+		public Sample(final String s, final boolean valid, final String comment) {
+			super();
+			this.str = s;
+			this.valid = valid;
+			this.comment = comment;
+		}
+
+		@Override
+		public String toString() {
+			return "'" + this.str + "' valid=" + this.valid + " \"" + this.comment + "\"";
+		}
+
+		String str;
+		boolean valid;
+		String comment;
+
+		public static Sample valid(final String s, final String comment) {
+			return new Sample(s, true, comment);
+		}
+
+		public static Sample invalid(final String s, final String comment) {
+			return new Sample(s, false, comment);
+		}
+	}
+
+	static String hundredChars = "a123456789" + "a123456789" + "a123456789" + "a123456789"
+			+ "a123456789" + "a123456789" + "a123456789" + "a123456789" + "a123456789"
+			+ "a123456789";
+
+	static {
+		assert hundredChars.length() == 100;
+	}
+
+	public static final Sample[] SAMPLES =
+
+		{
+
+				Sample.valid("s", "legal xml name"), Sample.valid("aaa", "legal xml name"),
+				Sample.valid("a", "a short one"),
+				Sample.valid("äöü", "some umlauts are legal in XML 1.0"),
+				Sample.invalid("a b", "fails if some regex uses a partial match"),
+				Sample.valid("aaa", "same again"),
+				Sample.invalid("aaa bbb", "fails if some regex uses a partial match"),
+				Sample.invalid(" ", "just a space"),
+				Sample.invalid("0a", "number at start is not allowed in XML 1.0"),
+				Sample.valid("Genußrechte", "ß is valid in XML 1.0"), Sample.valid("ß", "valid"),
+				Sample.invalid("========", "illegal in XML 1.0"),
+				Sample.invalid("a========", "illegal in XML 1.0"),
+				Sample.invalid("GenuÃrechte",
+						"??? contains unicode control character (sequence 195=C3,159=9F; this is UTF-8 encoded 'ß')"),
+				Sample.valid(hundredChars, "just right"),
+				Sample.invalid(hundredChars + "a", "too long"),
+				Sample.valid("_foo", "one leading underscores"),
+				Sample.valid("__foo", "two leading underscores"),
+				Sample.valid("___foo", "three leading underscores"), Sample.invalid(
+						"4*(var_1-min(o$8:o$49))/(max(o$8:o$49)-min(o$8:o$49))+1\"", "too long") };
+
+	public static void main(final String[] args) {
+
+
+		final String p = "=(var_0*var_1+var_2*var_3+var_4*var_5+var_6*var_7+var_8*var_9+var_10*var_11+var_12*var_13)/sum($var_1:$var_13)";
+		System.out.println(XidCodec.encode(p, 100));
+		System.out.println(XidCodec.decode(XidCodec.encode(p, 100)));
+
+
+		final String h = "GenuÃrechte";
+		System.out.println(h);
+		final String k = "4*(var_1-min(o$8:o$49))/(max(o$8:o$49)-min(o$8:o$49))+1\"";
+		System.out.println(XidCodec.encode(k, 500));
+
+		final String l = "=average(var_0:VAR_41)";
+		System.out.println(l);
+		System.out.println(XidCodec.encode(l, 200));
+		System.out.println(XidCodec.decode(XidCodec.encode(l, 200)));
+
+		System.out.println(XidCodec.decode(XidCodec.encode(k, 200)));
+
+		for (int i = 0; i < h.length(); i++) {
+			System.out.println("char[" + i + "]= " + h.charAt(i) + "=(" + (int) h.charAt(i)
+			+ ") codepoint='" + h.codePointAt(i) + "'");
+		}
+
+		System.out.println("159 = '" + (char) 159 + "' = a unicode control character");
+		System.out.println("159 = '" + (char) 159 + "'");
+
+		final StringBuilder b = new StringBuilder();
+		XidCodec.appendEncoded(b, 1, 100);
+		b.append(" | ");
+		XidCodec.appendEncoded(b, 1042, 100);
+		b.append(" | ");
+		XidCodec.appendEncoded(b, 65699, 100);
+		System.out.println(b);
+
+		System.out.println(XidCodec.encodeAsXId("40", XIdProvider.MAX_LENGTH));
+
+		final String[] s = new String[] {
+
+				"Self-organized+Reuse+of+Software+Engineering+Knowledge+Supported+by+Semantic+Wikis",
+		"CallAutomata?)>\\" };
+		for (final String a : s) {
+			System.out.println(XidCodec.encodeAsXId(a, XIdProvider.MAX_LENGTH));
+		}
+		System.out.println(XidCodec.decode("_30install"));
+		System.out.println(XidCodec.decode("_meta"));
+	}
+
+	@Test
+	public void testUsualSuspects() {
+		for (final Sample sample : SAMPLES) {
+			if (sample.valid) {
+				Assert.assertTrue("Should be valid '" + sample + "'",
+						MemoryStringIDProvider.isValidId(sample.str));
+			} else {
+				Assert.assertFalse("Should be invalid '" + sample + "'",
+						MemoryStringIDProvider.isValidId(sample.str));
+			}
+			if (sample.str.length() < 100) {
+				testEncodeDecode(sample.str);
+			}
+		}
+	}
+
+	@Test
+	public void testValid() {
+		assertValid("aaa");
+		assertValid("aaa");
+		assertValid("a");
+		assertValid("öäü");
+		assertValid("a");
+		assertInvalid("a b");
+		assertInvalid("aaa bbb");
+		assertInvalid(" ");
+		assertInvalid("0a");
+	}
+
+	private static void assertValid(final String s) {
+		Assert.assertTrue(s, MemoryStringIDProvider.isValidId(s));
+		Assert.assertTrue(s, gwt_matchesXydraId(s));
+	}
+
+	private static void assertInvalid(final String s) {
+		Assert.assertFalse(s, MemoryStringIDProvider.isValidId(s));
+		Assert.assertFalse(s, gwt_matchesXydraId(s));
+	}
+
+	private static final RegExp p = RegExp.compile(MemoryStringIDProvider.nameRegex);
+
+	// GWT version
+	public static boolean gwt_matchesXydraId(final String uriString) {
+		final MatchResult m = p.exec(uriString);
+		if (m == null)
+			return false;
+		final String match = m.getGroup(0);
+		return match != null && match.length() == uriString.length();
+	}
+
+	private static void testEncodeDecode(final String raw) {
+		final XId enc = XidCodec.encodeAsXId(raw, XIdProvider.MAX_LENGTH);
+		System.out.println("'" + raw + "'='" + enc + "'");
+		final String dec = XidCodec.decodeFromXId(enc);
+		Assert.assertEquals(raw, dec);
+	}
+
+}
