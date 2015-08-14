0811fe63978d4410e698c21385eee0e1883bcc7d
Max Völkel <max.voelkel@calpano.com>
2015-08-14 08:50:20 +0200

    conf: added util functions to interpret things as "true"

--- org.xydra.core/src/main/java/org/xydra/core/util/ConfigUtils.java
+++ org.xydra.core/src/main/java/org/xydra/core/util/ConfigUtils.java
@@ -1,5 +1,8 @@
 package org.xydra.core.util;
 
+import org.xydra.conf.ConfigException;
+import org.xydra.conf.IConfig;
+
 public class ConfigUtils {
 
 	/**
@@ -7,7 +10,34 @@ public class ConfigUtils {
 	 * @return true if string is not null and equals true (case ignored).
 	 */
 	public static boolean isTrue(final String booleanString) {
-		return booleanString != null && booleanString.equalsIgnoreCase("true");
+		return booleanString != null && (
+
+		booleanString.equalsIgnoreCase("true")
+
+		|| booleanString.equalsIgnoreCase("on")
+
+		|| booleanString.equalsIgnoreCase("yes")
+
+		);
+	}
+
+	/**
+	 * @param conf
+	 * @param key
+	 * @return true if a value is set and expresses "true"
+	 */
+	public static boolean isTrue(final IConfig conf, final String key) {
+		final Object o = conf.tryToGet(key);
+		if (o == null) {
+			return false;
+		}
+		if (o instanceof Boolean) {
+			return (Boolean) o;
+		}
+		if (o instanceof String) {
+			return isTrue((String) o);
+		}
+		throw new ConfigException("Expected null,String or boolean but found type " + o.getClass().getName());
 	}
 
 }
