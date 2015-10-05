4234fc5670be1ba5d57b900f5ea495376c66f68b
Max Völkel <max.voelkel@calpano.com>
2015-10-05 21:48:57 +0200

    EntrySets.merge -- fast for special cases

--- org.xydra.index/src/main/java/org/xydra/index/impl/EntrySets.java
+++ org.xydra.index/src/main/java/org/xydra/index/impl/EntrySets.java
@@ -8,12 +8,21 @@ public class EntrySets {
 	/**
 	 * This code is neither brilliant nor fast. But at least we have a central location for it.
 	 *
-	 * @param a
-	 * @param b
+	 * @param a @CanBeNull
+	 * @param b @CanBeNull
 	 * @param entrySetFactory
 	 * @return
 	 */
 	public static <E> IEntrySet<E> merge(final IEntrySet<E> a, final IEntrySet<E> b, final Factory<IEntrySet<E>> entrySetFactory) {
+		if (a == null) {
+			// no need to merge; @CanBeNull
+			return b;
+		} else if (b == null) {
+			// no need to merge
+			return a;
+		}
+
+		// merge
 		final IEntrySet<E> merged = entrySetFactory.createInstance();
 		for (final E e : a) {
 			merged.index(e);
