26995c06619aae44113b47284ee5c2f83ebe498a
Max Völkel <max.voelkel@calpano.com>
2015-10-11 15:15:45 +0200

    CollectionUtils: add toSortedList

--- org.xydra.index/src/main/java/org/xydra/index/CollectionUtils.java
+++ org.xydra.index/src/main/java/org/xydra/index/CollectionUtils.java
@@ -1,6 +1,11 @@
 package org.xydra.index;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class CollectionUtils {
@@ -40,6 +45,28 @@ public class CollectionUtils {
 			}
 		}
 		return true;
+	}
+
+	/**
+	 * @param coll
+	 * @return a sorted {@link ArrayList}
+	 */
+	public static <E extends Comparable<E>> List<E> toSortedList(final Collection<E> coll) {
+		final ArrayList<E> list = new ArrayList<>(coll.size());
+		list.addAll(coll);
+		Collections.sort(list);
+		return list;
+	}
+
+	/**
+	 * @param coll
+	 * @return a sorted {@link ArrayList}
+	 */
+	public static <E> List<E> toSortedList(final Collection<E> coll, final Comparator<E> comparator) {
+		final ArrayList<E> list = new ArrayList<>(coll.size());
+		list.addAll(coll);
+		Collections.sort(list, comparator);
+		return list;
 	}
 
 }
